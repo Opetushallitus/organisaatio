f83815635ff42b52d954c95b77469a05dde21ca6
Markus Veranen <markus.veranen@cybercom.com>
2014-01-16 10:13:31 +0200

    OH-227: Syötteen validointi

--- organisaatio-ui/src/main/webapp/js/directives/organisaatioDirectives.js
+++ organisaatio-ui/src/main/webapp/js/directives/organisaatioDirectives.js
@@ -2,6 +2,7 @@ app.directive('editAddress', function() {
     return {
         restrict: 'E',
         scope: {
+            form: '=',
             label: '=',
             addressmodelfi: '=',
             addressmodelsv: '=',
@@ -22,3 +23,64 @@ app.directive('editAddress', function() {
     };
 });
 
+app.directive('testField', function() {
+    return {
+        require: 'ngModel',
+        
+        link: function(scope, elm, attrs, ctrl) {
+            ctrl.$parsers.unshift(function(viewValue) {
+                console.log("ret " + viewValue);
+                return viewValue;
+            });
+        }
+    };
+});
+
+app.directive('namesCombinedField', function() {
+    return {
+        require: 'ngModel',
+
+        link: function(scope, elm, attrs, ctrl) {
+            ctrl.$parsers.unshift(function(viewValue) {
+                var returnUndefined = false;
+                scope.form.nimifi.$setValidity('namescombinedrequired', true);
+                
+                if (!scope.form.nimifi.$viewValue && !scope.form.nimisv.$viewValue && !scope.form.nimien.$viewValue) {
+                    scope.form.nimifi.$setValidity('namescombinedrequired', false);
+                    returnUndefined = true;
+                }                
+                if (returnUndefined === true) {
+                    return undefined;
+                } else {
+                    return viewValue;
+                }
+
+            });
+        }
+    };
+});
+
+app.directive('addressCombinedField', function() {
+    return {
+        require: 'ngModel',
+        
+        link: function(scope, elm, attrs, ctrl) {
+            ctrl.$parsers.unshift(function(viewValue) {
+                var returnUndefined = false;
+                scope.innerForm.osoitefi.$setValidity('addresscombinedrequired', true);
+                                
+                if (!(scope.innerForm.osoitefi.$viewValue && scope.innerForm.postifi.$viewValue) 
+                        && !(scope.innerForm.osoitesv.$viewValue && scope.innerForm.postisv.$viewValue )
+                        && !scope.innerForm.kvosoite.$viewValue) {
+                    scope.innerForm.osoitefi.$setValidity('addresscombinedrequired', false);
+                    returnUndefined = true;
+                }                
+                if (returnUndefined === true) {
+                    return viewValue; //return undefined;
+                } else {
+                    return viewValue;
+                }
+            });
+        }
+    };
+});
