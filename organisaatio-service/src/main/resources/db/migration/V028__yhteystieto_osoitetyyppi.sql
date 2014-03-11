66ca2cb609a61936cb38dd5ad09f3943df54e46b
Simo Kosunen <simo.kosunen@cybercom.com>
2014-03-11 14:55:19 +0200

    OVT-6875: Säilytetään sittenkin ulkomainen_posti ja ulkomainen_käynti.

--- organisaatio-service/src/main/resources/db/migration/V028__yhteystieto_osoitetyyppi.sql
+++ organisaatio-service/src/main/resources/db/migration/V028__yhteystieto_osoitetyyppi.sql
@@ -1,4 +1,2 @@
 update yhteystieto set osoitetyyppi = 'posti'  where osoitetyyppi = 'ruotsi_posti';
 update yhteystieto set osoitetyyppi = 'kaynti' where osoitetyyppi = 'ruotsi_kaynti';
-update yhteystieto set osoitetyyppi = 'posti'  where osoitetyyppi = 'ulkomainen_posti';
-update yhteystieto set osoitetyyppi = 'kaynti' where osoitetyyppi = 'ulkomainen_kaynti';
