bf9ab1f1446c38fca191038dae9937053e0abe93
Simo Kosunen <simo.kosunen@cybercom.com>
2014-03-11 14:16:15 +0200

    OVT-6875: Poistetaan yhteystieto-taulun osoitetyypeistä kieli (ruotsi_posti --> posti, ruotsi_kaynti --> kaynti...)

---
+++ organisaatio-service/src/main/resources/db/migration/V028__yhteystieto_osoitetyyppi.sql
@@ -0,0 +1,4 @@
+update yhteystieto set osoitetyyppi = 'posti'  where osoitetyyppi = 'ruotsi_posti';
+update yhteystieto set osoitetyyppi = 'kaynti' where osoitetyyppi = 'ruotsi_kaynti';
+update yhteystieto set osoitetyyppi = 'posti'  where osoitetyyppi = 'ulkomainen_posti';
+update yhteystieto set osoitetyyppi = 'kaynti' where osoitetyyppi = 'ulkomainen_kaynti';
