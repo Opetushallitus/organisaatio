39ce1f429b3a17ef47099a9ac85ae2773e51e7ba
Riku Salminen <riku.salminen@cybercom.com>
2014-05-08 13:27:47 +0300

    OVT-7450: filtteröidään IE 10:n generoimat ylimääräiset input-eventit pois kts. https://github.com/angular/angular.js/issues/2614

--- organisaatio-ui/src/main/webapp/js/app.js
+++ organisaatio-ui/src/main/webapp/js/app.js
@@ -81,6 +81,21 @@ app.config(function($routeProvider, $httpProvider) {
         otherwise({redirectTo:'/organisaatiot'});
 });
 
+// https://github.com/angular/angular.js/issues/2614
+app.config(['$provide', function($provide) {
+    $provide.decorator('$sniffer', ['$delegate', function($sniffer) {
+            var msie = parseInt((/msie (\d+)/.exec(angular.lowercase(navigator.userAgent)) || [])[1], 10);
+            var _hasEvent = $sniffer.hasEvent;
+            $sniffer.hasEvent = function(event) {
+                if (event === 'input' && msie === 10) {
+                    return false;
+                }
+                _hasEvent.call(this, event);
+            };
+            return $sniffer;
+        }]);
+}]);
+
 app.run(function(OrganisaatioInitAuth, UserInfo) {
     // Tehdään autentikoitu get servicelle
     // Näin kierretään ongelma: "CAS + ensimmäinen autentikoitia vaativa POST kutsu"
