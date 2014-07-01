13214fe4305e4bff3a9d6426be8ed941028b0774
Simo Kosunen <simo.kosunen@cybercom.com>
2014-07-01 10:06:19 +0300

    OVT-5319: Haetaan koulutukset tarjonnalta (deserialisointi result generic luokkarakenteeseen = NULL)

--- organisaatio-service/src/main/java/fi/vm/sade/organisaatio/business/impl/OrganisaatioKoulutukset.java
+++ organisaatio-service/src/main/java/fi/vm/sade/organisaatio/business/impl/OrganisaatioKoulutukset.java
@@ -15,9 +15,6 @@
 
 package fi.vm.sade.organisaatio.business.impl;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import fi.vm.sade.generic.common.EnhancedProperties;
 import fi.vm.sade.generic.rest.CachingRestClient;
 import fi.vm.sade.organisaatio.business.exception.OrganisaatioTarjontaException;
@@ -25,28 +22,34 @@ import fi.vm.sade.tarjonta.service.resources.v1.dto.HakutuloksetV1RDTO;
 import fi.vm.sade.tarjonta.service.resources.v1.dto.KoulutusHakutulosV1RDTO;
 import fi.vm.sade.tarjonta.service.resources.v1.dto.ResultV1RDTO;
 import fi.vm.sade.tarjonta.service.resources.v1.dto.ResultV1RDTO.ResultStatus;
-
+import fi.vm.sade.tarjonta.service.resources.v1.dto.TarjoajaHakutulosV1RDTO;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
 import java.util.Properties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
  * @author simok
  */
 public class OrganisaatioKoulutukset {
-    
+
     private final Logger LOG = LoggerFactory.getLogger(getClass());
-    
+
     // NOTE! cachingRestClient is static because we need application-scoped rest cache for organisaatio-service
     private static final CachingRestClient cachingRestClient = new CachingRestClient();
     private String tarjontaServiceWebappUrl;
-    
+
     public OrganisaatioKoulutukset(String tarjontaServiceWebappUrl) {
         this.tarjontaServiceWebappUrl = tarjontaServiceWebappUrl;
     }
-    
+
     public OrganisaatioKoulutukset() {
         // read tarjonta-service url from common.properties
         FileInputStream fis = null;
@@ -65,38 +68,85 @@ public class OrganisaatioKoulutukset {
             throw new RuntimeException("failed to read common.properties", e);
         }
     }
-    
+
     private <T> T get(String uri, Class<? extends T> resultClass) {
         try {
             long t0 = System.currentTimeMillis();
             T result = cachingRestClient.get(tarjontaServiceWebappUrl + "/rest/v1" + uri, resultClass);
-            LOG.debug("tarjonta rest get done, uri: {}, took: {} ms, cacheStatus: {} result: {}", 
+            LOG.debug("tarjonta rest get done, uri: {}, took: {} ms, cacheStatus: {} result: {}",
                     new Object[] { uri, (System.currentTimeMillis() - t0), cachingRestClient.getCacheStatus(), result });
             return result;
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
-    
+
     private String buildSearchKoulutusUri(String oid) {
         return "/koulutus/search?" + "organisationOid=" + oid;
     }
-    
-    public boolean haeKoulutukset(String oid) {
-        LOG.debug("Haetaan koulutuksia oidille: " + oid);
-        
-        ResultV1RDTO<HakutuloksetV1RDTO<KoulutusHakutulosV1RDTO>> hakuTulokset;
 
+    private List<KoulutusHakutulosV1RDTO> haeKoulutukset(String oid) {
+        LOG.debug("Haetaan koulutuksia oidille: " + oid);
+
+        ResultV1RDTO<HakutuloksetV1RDTO<KoulutusHakutulosV1RDTO>> hakuTulokset;
         ResultV1RDTO<HakutuloksetV1RDTO<KoulutusHakutulosV1RDTO>> classInstance = new ResultV1RDTO<HakutuloksetV1RDTO<KoulutusHakutulosV1RDTO>>();
-                
+
+
         hakuTulokset = get(buildSearchKoulutusUri(oid), classInstance.getClass());
-        
+
         // Tarkistetaan hakutulokset
         if (hakuTulokset.getStatus() != ResultStatus.OK) {
             LOG.warn("Search failed for koulutus with organization oid: " + oid + " status: " + hakuTulokset.getStatus());
             throw new OrganisaatioTarjontaException();
         }
-        
-        return true;
+
+        LOG.debug("Hakutulokset: "+ hakuTulokset.toString());
+
+        if (hakuTulokset.getResult() == null) {
+            LOG.debug("Hakutulosten result NULL!");
+            return Collections.EMPTY_LIST;
+        }
+
+        List<TarjoajaHakutulosV1RDTO<KoulutusHakutulosV1RDTO>> tarjoajienKoulutukset = hakuTulokset.getResult().getTulokset();
+        List<KoulutusHakutulosV1RDTO> koulutukset = new ArrayList<KoulutusHakutulosV1RDTO>();
+
+        // Ei löytynyt koulutuksia
+        if (hakuTulokset.getResult().getTuloksia() == 0 || tarjoajienKoulutukset == null) {
+            LOG.debug("Ei koulutuksia organisaatiolle: " + oid);
+            return Collections.EMPTY_LIST;
+        }
+
+        // Kerätään tuloksista annetun oid:n määrittämän tarjoajan koulutukset
+        for (TarjoajaHakutulosV1RDTO<KoulutusHakutulosV1RDTO> tarjoajanKoulutukset: tarjoajienKoulutukset) {
+            // Ei pitäisi olla muita tarjoajia listalla, mutta tarkistetaan silti
+            if (tarjoajanKoulutukset.getOid().equals(oid)) {
+		koulutukset.addAll(tarjoajanKoulutukset.getTulokset());
+            }
+	}
+
+        return koulutukset;
     }
-}
+
+    public boolean alkaviaKoulutuksia(String oid, Date after) {
+        List<KoulutusHakutulosV1RDTO> koulutukset  = haeKoulutukset(oid);
+
+        // Ei alkavia koulutuksia
+        if (koulutukset.isEmpty()) {
+            return false;
+        }
+
+        // Tarkistetaan onko alkavia koulutuksia annetun päivämäärän jälkeen
+        for (KoulutusHakutulosV1RDTO koulutus : koulutukset) {
+            if (koulutus.getKoulutuksenAlkamisPvmMax().after(after)) {
+                LOG.debug("After: " + koulutus.getNimi().get("fi"));
+		return true;
+            }
+            else {
+                LOG.debug("Before: " + koulutus.getNimi().get("fi"));
+            }
+	}
+
+        // Ei alkavia koulutuksia annetun päivämäärän jälkeen
+        return false;
+    }
+ }
