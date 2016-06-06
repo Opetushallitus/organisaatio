c1f9ae4d318c45a54eba6f4019bef39425c4fce0
Marja Kari <marja.kari@iki.fi>
2016-06-06 18:29:16 +0300

    OH-380 classes and tables for ytj mass update error collecting, initial draft

---
+++ organisaatio-service/src/main/resources/db/migration/V0078__ytj_mass_update_status_tables.sql
@@ -0,0 +1,28 @@
+CREATE TABLE ytjpaivitysloki (
+  id              INT8         NOT NULL UNIQUE,
+  version         INT8         NOT NULL,
+  paivitysaika    TIMESTAMP    NOT NULL,
+  paivitetyt_lkm  INT8,
+  paivitys_tila   VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE ytjvirhe (
+  id              INT8         NOT NULL UNIQUE,
+  version         INT8         NOT NULL,
+  oid             VARCHAR(255) NOT NULL,
+  ytjpaivitysloki_id INT8      NOT NULL,
+  virhekentta     VARCHAR(255),
+  virheviesti   VARCHAR(255),
+  PRIMARY KEY (id)
+);
+
+ALTER TABLE ytjvirhe
+ADD CONSTRAINT ytjvirhe_ytjpaivitys
+FOREIGN KEY (ytjpaivitysloki_id)
+REFERENCES ytjpaivitysloki;
+
+CREATE INDEX ytjvirhe_oid_idx ON ytjvirhe (oid);
+CREATE INDEX ytjpaivitysloki_paivitysaika_idx ON ytjpaivitysloki (paivitysaika);
+
+
