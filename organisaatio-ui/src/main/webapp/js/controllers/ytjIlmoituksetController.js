9538d9c2ad210693cf608fbb742c672fa0b4a681
Petri Ryhänen <petri.ryhanen@csc.fi>
2016-06-15 10:10:42 +0300

    OH-387: Add return button. Add ophcrud level authentication requirement to view.

--- organisaatio-ui/src/main/webapp/js/controllers/ytjIlmoituksetController.js
+++ organisaatio-ui/src/main/webapp/js/controllers/ytjIlmoituksetController.js
@@ -1,5 +1,21 @@
-app.controller('YtjIlmoituksetController', ['$scope', '$log', 'YtjLoki', 'LocalisationService',
-    function YtjIlmoituksetController($scope, $log, YtjLoki, LocalisationService) {
+/*
+ Copyright (c) 2014 The Finnish National Board of Education - Opetushallitus
+
+ This program is free software:  Licensed under the EUPL, Version 1.1 or - as
+ soon as they will be approved by the European Commission - subsequent versions
+ of the EUPL (the "Licence");
+
+ You may not use this work except in compliance with the Licence.
+ You may obtain a copy of the Licence at: http://www.osor.eu/eupl/
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ European Union Public Licence for more details.
+ */
+
+app.controller('YtjIlmoituksetController', ['$scope', '$log', '$location', 'YtjLoki', 'LocalisationService',
+    function YtjIlmoituksetController($scope, $log, $location, YtjLoki, LocalisationService) {
         $log = $log.getInstance("YtjIlmoituksetController");
         $scope.logs = [];
 
@@ -39,6 +55,11 @@ app.controller('YtjIlmoituksetController', ['$scope', '$log', 'YtjLoki', 'Locali
             else {
                 $log.error('Invalid paivitysTila');
             }
-        }
+        };
+
+        // Siirtyminen organisaatioiden pääsivulle organisaatiopuu näkymään
+        $scope.cancel = function() {
+            $location.path("/");
+        };
     }
 ]);
