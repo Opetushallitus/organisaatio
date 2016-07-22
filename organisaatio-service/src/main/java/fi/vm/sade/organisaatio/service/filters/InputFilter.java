18d5cfcd4c5457573155a661d968062b80c95cae
Petri Ryhänen <petri.ryhanen@csc.fi>
2016-07-22 11:51:04 +0300

    Change cxf filters to jaxrs filters.

--- organisaatio-service/src/main/java/fi/vm/sade/organisaatio/service/filters/InputFilter.java
+++ organisaatio-service/src/main/java/fi/vm/sade/organisaatio/service/filters/InputFilter.java
@@ -1,37 +1,31 @@
 package fi.vm.sade.organisaatio.service.filters;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.core.Response;
-import org.apache.cxf.jaxrs.model.ClassResourceInfo;
-import org.apache.cxf.message.Message;
-//import org.apache.cxf.jaxrs.ext.RequestHandler;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.PreMatching;
+import javax.ws.rs.ext.Provider;
+import org.apache.cxf.phase.PhaseInterceptorChain;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.web.servletapi.SecurityContextHolderAwareRequestWrapper;
+import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 
+@Component
+@Provider
+@PreMatching
 // Saves the ID chain from incoming REST messages for outputfilter. Loadbalancer adds this ID to messages automatically.
-//public class InputFilter implements RequestHandler {
-//    private static final Logger LOG = LoggerFactory.getLogger(InputFilter.class);
-//    static int runner = 0;
-//
-//    @Override
-//    public Response handleRequest(Message inMessage, ClassResourceInfo classResourceInfo) {
-//        // Try to find IDCain from message header.
-//        String IDChain = null;
-//        HttpServletRequest request = null;
-//        SecurityContextHolderAwareRequestWrapper wrapper = (SecurityContextHolderAwareRequestWrapper)inMessage.get("HTTP.REQUEST");
-//        if(wrapper != null) {
-//            request = (HttpServletRequest)wrapper.getRequest();
-//        }
-//        if(request != null) {
-//            IDChain = request.getHeader("ID");
-//        }
-//        // Save the ID chain to cxf exchange.
-//        if(IDChain != null) {
-//            inMessage.getExchange().put("ID", IDChain);
-//        }
-//        // Continue the process.
-//        return null;
-//    }
-//}
+public class InputFilter implements ContainerRequestFilter {
+    private static final Logger LOG = LoggerFactory.getLogger(InputFilter.class);
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        // Try to find IDCain from message header.
+        String IDChain = requestContext.getHeaderString("ID");
+        // Save the ID chain to cxf exchange.
+        if(IDChain != null) {
+            PhaseInterceptorChain.getCurrentMessage().getExchange().put("ID", IDChain);
+        }
+    }
+}
--- organisaatio-service/src/main/java/fi/vm/sade/organisaatio/service/filters/OutputFilter.java
+++ organisaatio-service/src/main/java/fi/vm/sade/organisaatio/service/filters/OutputFilter.java
@@ -1,26 +1,37 @@
 package fi.vm.sade.organisaatio.service.filters;
 
-//import org.apache.cxf.jaxrs.ext.ResponseHandler;
-import org.apache.cxf.jaxrs.impl.ResponseImpl;
-import org.apache.cxf.jaxrs.model.OperationResourceInfo;
-import org.apache.cxf.message.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import javax.ws.rs.core.Response;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.container.PreMatching;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.Provider;
+
+import org.apache.cxf.phase.PhaseInterceptorChain;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 // Pass on the received ID Chain intact. Load balancer will update this ID chain automatically. Add the same "Caller-Id"
 // (clientSubSystemCode) to every sent REST message.
-//public class OutputFilter implements ResponseHandler {
-//    private static final Logger LOG = LoggerFactory.getLogger(OutputFilter.class);
-//
-//    @Override
-//    public Response handleResponse(Message outMessage, OperationResourceInfo operationResourceInfo, Response response) {
-//        String IDChain = (String)outMessage.getExchange().get("ID");
-//        // Add callerid and ID chain headers to the output message.
-//        if(response != null) {
-//            ((ResponseImpl)response).getHeaders().add("ID", IDChain);
-//            ((ResponseImpl)response).getHeaders().add("clientSubSystemCode", IDContextMessageHelper.getClientSubSystemCode());
-//        }
-//        return response;
-//    }
-//}
+@Component
+@Provider
+@PreMatching
+public class OutputFilter implements ContainerResponseFilter {
+    private static final Logger LOG = LoggerFactory.getLogger(OutputFilter.class);
+
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+        String IDChain = (String)PhaseInterceptorChain.getCurrentMessage().getExchange().get("ID");
+
+        // Add callerid and ID chain headers to the output message.
+        if(responseContext != null && responseContext.getHeaders() != null) {
+            MultivaluedMap<String, Object> responseHeaders = responseContext.getHeaders();
+            responseHeaders.add("ID", IDChain);
+            responseHeaders.add("clientSubSystemCode", IDContextMessageHelper.getClientSubSystemCode());
+        }
+    }
+}
