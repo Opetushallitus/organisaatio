4ab093478922428920d8a706e883c28fd62e98aa
Risto Autio <risto.autio@dicode.fi>
2016-10-06 13:22:32 +0300

    added missing properties

--- organisaatio-service/src/main/java/fi/vm/sade/organisaatio/config/UrlConfiguration.java
+++ organisaatio-service/src/main/java/fi/vm/sade/organisaatio/config/UrlConfiguration.java
@@ -1,6 +1,8 @@
 package fi.vm.sade.organisaatio.config;
 
 import fi.vm.sade.properties.OphProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 
 import java.nio.file.Paths;
@@ -10,9 +12,13 @@ import java.nio.file.Paths;
  */
 @Configuration
 public class UrlConfiguration extends OphProperties {
+    private static final Logger LOG = LoggerFactory.getLogger(UrlConfiguration.class);
+
     public UrlConfiguration() {
         addFiles("/organisaatio-service-oph.properties");
         addOptionalFiles(Paths.get(System.getProperties().getProperty("user.home"), "/oph-configuration/common.properties").toString());
         addOptionalFiles("/organisaatio-service-test.properties");
+
+        LOG.info("property: 'organisaatio-service.ryhmasahkoposti-service.rest.url': " + getProperty("organisaatio-service.ryhmasahkoposti-service.rest.url"));
     }
 }
