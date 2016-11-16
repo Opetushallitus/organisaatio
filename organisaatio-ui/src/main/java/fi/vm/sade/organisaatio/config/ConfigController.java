659730ae16ef69d3614b3ba774aaa4601fb02d09
Mikko Apo <mikko.apo@reaktor.fi>
2016-11-16 11:20:21 +0200

    clearing oph.properties to get actual service as the first element in the url key

--- organisaatio-ui/src/main/java/fi/vm/sade/organisaatio/config/ConfigController.java
+++ organisaatio-ui/src/main/java/fi/vm/sade/organisaatio/config/ConfigController.java
@@ -24,12 +24,12 @@ public class ConfigController {
     public String index() {
         StringBuilder b = new StringBuilder();
         append(b, "UI_URL_BASE", ophProperties.getProperty("organisaatio-ui.organisaatio-ui-url"));
-        append(b, "SERVICE_URL_BASE", ophProperties.getProperty("organisaatio-ui.organisaatio-service-url.rest"));
-        append(b, "KOODISTO_URL_BASE", ophProperties.getProperty("organisaatio-ui.koodisto-service-url.rest"));
-        append(b, "LOKALISAATIO_URL_BASE", ophProperties.getProperty("organisaatio-ui.lokalisaatio-service-url.rest"));
-        append(b, "V1_LOKALISAATIO_URL", ophProperties.getProperty("organisaatio-ui.lokalisaatio-service-url.lokalisointi.v1"));
+        append(b, "SERVICE_URL_BASE", ophProperties.getProperty("organisaatio-service.url.rest"));
+        append(b, "KOODISTO_URL_BASE", ophProperties.getProperty("koodisto-service.url.rest"));
+        append(b, "LOKALISAATIO_URL_BASE", ophProperties.getProperty("lokalisaatio-service.url.rest"));
+        append(b, "V1_LOKALISAATIO_URL", ophProperties.getProperty("lokalisaatio-service.url.lokalisointi.v1"));
 
-        append(b, "AUTHENTICATION_URL_BASE", ophProperties.getProperty("organisaatio-ui.authentication-service-url.rest"));
+        append(b, "AUTHENTICATION_URL_BASE", ophProperties.getProperty("authentication-service-url.rest"));
         append(b, "ROOT_ORGANISAATIO_OID", ophProperties.getProperty("root.organisaatio.oid"));
 
         append(b, "TEMPLATE_URL_BASE", "");
@@ -39,52 +39,52 @@ public class ConfigController {
         append(b, "SESSION_KEEPALIVE_INTERVAL_IN_SECONDS", ophProperties.getOrElse("organisaatio-ui.session-keepalive-interval.seconds", "30"));
         append(b, "MAX_SESSION_IDLE_TIME_IN_SECONDS", ophProperties.getOrElse("organisaatio-ui.max-session-idle-time.seconds", "1800"));
 
-        append(b, "ORGANISAATIO_REST", ophProperties.getProperty("organization-service.rest.organisaatio"));
-        append(b, "ORGANISAATIO_REST_HAE", ophProperties.getProperty("organization-service.rest.organisaatio.param", "hae"));
-        append(b, "ORGANISAATIO_REST_BY_OID", ophProperties.getProperty("organization-service.rest.organisaatio.param", ":oid"));
+        append(b, "ORGANISAATIO_REST", ophProperties.getProperty("organisaatio-service.rest.organisaatio"));
+        append(b, "ORGANISAATIO_REST_HAE", ophProperties.getProperty("organisaatio-service.rest.organisaatio.param", "hae"));
+        append(b, "ORGANISAATIO_REST_BY_OID", ophProperties.getProperty("organisaatio-service.rest.organisaatio.param", ":oid"));
 
-        append(b, "ORGANISAATIO_REST_ORGAISAATIO_MAXINACTIVEINTERVAL", ophProperties.getProperty("organization-service.rest.organisaatio.session.maxinactiveinterval"));
-        append(b, "ORGANISAATIO_REST_YTJ_LOKI", ophProperties.getProperty("organization-service.rest.organisaatio.ytjloki"));
-
-
-        append(b, "ORGANISAATIO_REST_V2_HAE", ophProperties.getProperty("organization-service.rest.organisaatio.v2.param", "hae"));
-        append(b, "ORGANISAATIO_REST_V2_RYHMAT", ophProperties.getProperty("organization-service.rest.organisaatio.v2.param", "ryhmat"));
-        append(b, "ORGANISAATIO_REST_V2_MUOKKAAMONTA", ophProperties.getProperty("organization-service.rest.organisaatio.v2.param", "muokkaamonta"));
+        append(b, "ORGANISAATIO_REST_ORGAISAATIO_MAXINACTIVEINTERVAL", ophProperties.getProperty("organisaatio-service.rest.organisaatio.session.maxinactiveinterval"));
+        append(b, "ORGANISAATIO_REST_YTJ_LOKI", ophProperties.getProperty("organisaatio-service.rest.organisaatio.ytjloki"));
 
 
-        append(b, "ORGANISAATIO_REST_V2_OID_HISTORIA", ophProperties.getProperty("organization-service.rest.organisaatio.v2.params", ":oid", "historia"));
-        append(b, "ORGANISAATIO_REST_V2_OID_ORGANISAATIOSUHDE", ophProperties.getProperty("organization-service.rest.organisaatio.v2.params", ":oid", "organisaatiosuhde"));
-        append(b, "ORGANISAATIO_REST_V2_HIERARKIA_HAE", ophProperties.getProperty("organization-service.rest.organisaatio.v2.params", "hierarkia", "hae"));
-        append(b, "ORGANISAATIO_REST_V2_PAIVITTAJA_HAE", ophProperties.getProperty("organization-service.rest.organisaatio.v2.params", ":oid", "paivittaja"));
-        append(b, "ORGANISAATIO_REST_V2_NIMIHISTORIA_HAE", ophProperties.getProperty("organization-service.rest.organisaatio.v2.nimihistoria.haku"));
-
-        append(b, "ORGANISAATIO_REST_AUTH", ophProperties.getProperty("organization-service.rest.organisaatio.param", "auth"));
-        append(b, "ORGANISAATIO_REST_YTJ_YTUNNUS", ophProperties.getProperty("organization-service.rest.organisaatio.ytj", ":ytunnus"));
-//        append(b, "ORGANISAATIO_REST_YTJ_HAE", ophProperties.getProperty("organization-service.rest.organisaatio.ytj", "hae"));
-        append(b, "ORGANISAATIO_REST_YTJ_HAE", ophProperties.getProperty("organization-service.rest.organisaatio.ytjHae"));
-
-//        append(b, "ORGANISAATIO_REST_YHTEYSTIETOJENTYYPPI", ophProperties.getProperty("organization-service.rest.organisaatio.yhteystietojentyyppi"));
-        append(b, "ORGANISAATIO_REST_YHTEYSTIETOJENTYYPPI", ophProperties.getProperty("organization-service.rest.organisaatio.yhteystietojentyyppi"));
-
-        append(b, "ORGANISAATIO_REST_YHTEYSTIETOJENTYYPPI_BY_OID", ophProperties.getProperty("organization-service.rest.organisaatio.yhteystietojentyyppiByOid", ":oid"));
-        append(b, "ORGANISAATIO_REST_YTJ_LOKI", ophProperties.getProperty("organization-service.rest.organisaatio.ytjloki"));
-
-        append(b, "AUTHENTICATION_REST_HENKILO", ophProperties.getProperty("organisaatio-ui.authentication-service.henkilo"));
-        append(b, "AUTHENTICATION_REST_HENKILO_BY_OID", ophProperties.getProperty("organisaatio-ui.authentication-service.henkilo.byOid"));
-        append(b, "AUTHENTICATION_REST_RYHMA_BY_HENKILO_OID", ophProperties.getProperty("organisaatio-ui.authentication-service.henkilo.ryhma"));
+        append(b, "ORGANISAATIO_REST_V2_HAE", ophProperties.getProperty("organisaatio-service.rest.organisaatio.v2.param", "hae"));
+        append(b, "ORGANISAATIO_REST_V2_RYHMAT", ophProperties.getProperty("organisaatio-service.rest.organisaatio.v2.param", "ryhmat"));
+        append(b, "ORGANISAATIO_REST_V2_MUOKKAAMONTA", ophProperties.getProperty("organisaatio-service.rest.organisaatio.v2.param", "muokkaamonta"));
 
 
-        append(b, "KOODISTO_ORGANISAATIOTYYPPI_KOODI", ophProperties.getProperty("organisaatio-ui.koodisto-service-url.rest.json", "organisaatiotyyppi", "koodi"));
-        append(b, "KOODISTO_OPPILAITOSTYYPPI_KOODI", ophProperties.getProperty("organisaatio-ui.koodisto-service-url.rest.json", "oppilaitostyyppi", "koodi"));
-        append(b, "KOODISTO_MAAT_JA_VALTIOT", ophProperties.getProperty("organisaatio-ui.koodisto-service-url.rest.json", "maatjavaltiot1", "koodi"));
-        append(b, "KOODISTO_KIELI_KOODI", ophProperties.getProperty("organisaatio-ui.koodisto-service-url.rest.json", "kieli", "koodi"));
-        append(b, "KOODISTO_VUOSILUOKAT", ophProperties.getProperty("organisaatio-ui.koodisto-service-url.rest.json", "vuosiluokat", "koodi"));
-        append(b, "KOODISTO_URI_KOODI", ophProperties.getProperty("organisaatio-ui.koodisto-service-url.rest.json", ":uri", "koodi"));
-        append(b, "KOODISTO_POSTI_KOODI", ophProperties.getProperty("organisaatio-ui.koodisto-service-url.rest.json", "posti", "koodi"));
-        append(b, "KOODISTO_KUNTA_KOODI", ophProperties.getProperty("organisaatio-ui.koodisto-service-url.rest.kunta.koodi"));
-        append(b, "KOODISTO_KOODI_HAE", ophProperties.getProperty("organisaatio-ui.koodisto-service-url.rest.search.koodis"));
-        append(b, "KOODISTO_POSTI", ophProperties.getProperty("organisaatio-ui.koodisto-service-url.rest.posti"));
-        append(b, "KOODISTO_OPPILAITOKSENOPETUSKIELI", ophProperties.getProperty("organisaatio-ui.koodisto-service-url.rest.oppilaitoksenopetuskieli.koodi"));
+        append(b, "ORGANISAATIO_REST_V2_OID_HISTORIA", ophProperties.getProperty("organisaatio-service.rest.organisaatio.v2.params", ":oid", "historia"));
+        append(b, "ORGANISAATIO_REST_V2_OID_ORGANISAATIOSUHDE", ophProperties.getProperty("organisaatio-service.rest.organisaatio.v2.params", ":oid", "organisaatiosuhde"));
+        append(b, "ORGANISAATIO_REST_V2_HIERARKIA_HAE", ophProperties.getProperty("organisaatio-service.rest.organisaatio.v2.params", "hierarkia", "hae"));
+        append(b, "ORGANISAATIO_REST_V2_PAIVITTAJA_HAE", ophProperties.getProperty("organisaatio-service.rest.organisaatio.v2.params", ":oid", "paivittaja"));
+        append(b, "ORGANISAATIO_REST_V2_NIMIHISTORIA_HAE", ophProperties.getProperty("organisaatio-service.rest.organisaatio.v2.nimihistoria.haku"));
+
+        append(b, "ORGANISAATIO_REST_AUTH", ophProperties.getProperty("organisaatio-service.rest.organisaatio.param", "auth"));
+        append(b, "ORGANISAATIO_REST_YTJ_YTUNNUS", ophProperties.getProperty("organisaatio-service.rest.organisaatio.ytj", ":ytunnus"));
+//        append(b, "ORGANISAATIO_REST_YTJ_HAE", ophProperties.getProperty("organisaatio-service.rest.organisaatio.ytj", "hae"));
+        append(b, "ORGANISAATIO_REST_YTJ_HAE", ophProperties.getProperty("organisaatio-service.rest.organisaatio.ytjHae"));
+
+//        append(b, "ORGANISAATIO_REST_YHTEYSTIETOJENTYYPPI", ophProperties.getProperty("organisaatio-service.rest.organisaatio.yhteystietojentyyppi"));
+        append(b, "ORGANISAATIO_REST_YHTEYSTIETOJENTYYPPI", ophProperties.getProperty("organisaatio-service.rest.organisaatio.yhteystietojentyyppi"));
+
+        append(b, "ORGANISAATIO_REST_YHTEYSTIETOJENTYYPPI_BY_OID", ophProperties.getProperty("organisaatio-service.rest.organisaatio.yhteystietojentyyppiByOid", ":oid"));
+        append(b, "ORGANISAATIO_REST_YTJ_LOKI", ophProperties.getProperty("organisaatio-service.rest.organisaatio.ytjloki"));
+
+        append(b, "AUTHENTICATION_REST_HENKILO", ophProperties.getProperty("authentication-service.henkilo"));
+        append(b, "AUTHENTICATION_REST_HENKILO_BY_OID", ophProperties.getProperty("authentication-service.henkilo.byOid"));
+        append(b, "AUTHENTICATION_REST_RYHMA_BY_HENKILO_OID", ophProperties.getProperty("authentication-service.henkilo.ryhma"));
+
+
+        append(b, "KOODISTO_ORGANISAATIOTYYPPI_KOODI", ophProperties.getProperty("koodisto-service.url.rest.json", "organisaatiotyyppi", "koodi"));
+        append(b, "KOODISTO_OPPILAITOSTYYPPI_KOODI", ophProperties.getProperty("koodisto-service.url.rest.json", "oppilaitostyyppi", "koodi"));
+        append(b, "KOODISTO_MAAT_JA_VALTIOT", ophProperties.getProperty("koodisto-service.url.rest.json", "maatjavaltiot1", "koodi"));
+        append(b, "KOODISTO_KIELI_KOODI", ophProperties.getProperty("koodisto-service.url.rest.json", "kieli", "koodi"));
+        append(b, "KOODISTO_VUOSILUOKAT", ophProperties.getProperty("koodisto-service.url.rest.json", "vuosiluokat", "koodi"));
+        append(b, "KOODISTO_URI_KOODI", ophProperties.getProperty("koodisto-service.url.rest.json", ":uri", "koodi"));
+        append(b, "KOODISTO_POSTI_KOODI", ophProperties.getProperty("koodisto-service.url.rest.json", "posti", "koodi"));
+        append(b, "KOODISTO_KUNTA_KOODI", ophProperties.getProperty("koodisto-service.url.rest.kunta.koodi"));
+        append(b, "KOODISTO_KOODI_HAE", ophProperties.getProperty("koodisto-service.url.rest.search.koodis"));
+        append(b, "KOODISTO_POSTI", ophProperties.getProperty("koodisto-service.url.rest.posti"));
+        append(b, "KOODISTO_OPPILAITOKSENOPETUSKIELI", ophProperties.getProperty("koodisto-service.url.rest.oppilaitoksenopetuskieli.koodi"));
 
 
         if (ophProperties.getProperty("auth.mode") != null && !ophProperties.getProperty("auth.mode").isEmpty()) {
