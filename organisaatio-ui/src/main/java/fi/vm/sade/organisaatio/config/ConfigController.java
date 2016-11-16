58a44ad662d1b9d5170c5f6d4c6c2b6fd7962e88
Mikko Apo <mikko.apo@reaktor.fi>
2016-11-16 11:40:02 +0200

    lokalisaatio-service > lokalisointi

--- organisaatio-ui/src/main/java/fi/vm/sade/organisaatio/config/ConfigController.java
+++ organisaatio-ui/src/main/java/fi/vm/sade/organisaatio/config/ConfigController.java
@@ -26,7 +26,7 @@ public class ConfigController {
         append(b, "UI_URL_BASE", ophProperties.getProperty("organisaatio-ui.organisaatio-ui-url"));
         append(b, "SERVICE_URL_BASE", ophProperties.getProperty("organisaatio-service.url.rest"));
         append(b, "KOODISTO_URL_BASE", ophProperties.getProperty("koodisto-service.url.rest"));
-        append(b, "LOKALISAATIO_URL_BASE", ophProperties.getProperty("lokalisaatio-service.url.rest"));
+        append(b, "LOKALISAATIO_URL_BASE", ophProperties.getProperty("lokalisointi.url.rest"));
         append(b, "V1_LOKALISAATIO_URL", ophProperties.getProperty("lokalisaatio-service.url.lokalisointi.v1"));
 
         append(b, "AUTHENTICATION_URL_BASE", ophProperties.getProperty("authentication-service-url.rest"));
