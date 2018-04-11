35e9987b3f09e3de1dcc3ea0a191dc8dc989e66c
Tapio Jaakkola <tapio.jaakkola@github.mail.kapsi.fi>
2018-04-11 10:39:18 +0300

    OH-335: Lisätään ryhmälle passivointi/aktivointi toiminallisuus

--- organisaatio-ui/src/main/webapp/js/controllers/ryhmienHallintaController.js
+++ organisaatio-ui/src/main/webapp/js/controllers/ryhmienHallintaController.js
@@ -54,6 +54,7 @@ app.controller('RyhmienHallintaController', function RyhmienHallintaController($
 
     $scope.valitseRyhma = function(group) {
         $scope.currentGroup = group;
+        console.log(group);
         $scope.updateUpdateInfo();
     };
 
@@ -102,6 +103,55 @@ app.controller('RyhmienHallintaController', function RyhmienHallintaController($
                 $log.debug('Ryhmän poisto peruttu');
             });
         }
+    };
+
+    $scope.disabloi = function() {
+        $log.info("disabloi(): ", $scope.currentGroup);
+        var previousDate = moment().subtract(1, 'day').format('YYYY-MM-DD');
+        $scope.currentGroup.lakkautusPvm = previousDate;
+
+        var deferred = $q.defer();
+
+        if ($scope.currentGroup !== null) {
+            $scope.model.save($scope.currentGroup, function(savedGroup) {
+                $scope.currentGroup = savedGroup;
+                $scope.updateUpdateInfo();
+                $scope.currentGroupSelection = {};
+                $scope.currentGroupSelection.selected = savedGroup;
+                $scope.form.$setPristine();
+                deferred.resolve();
+            }, function(error) {
+                loadingService.onErrorHandled(error);
+                $log.warn("Failed to save group: ", $scope.currentGroup);
+                Alert.add("error", $filter('i18n')(error.data.errorKey || 'generic.error'), false);
+                deferred.reject();
+            });
+        }
+        return deferred.promise;
+    };
+
+    $scope.enabloi = function() {
+        $log.info("enabloi(): ", $scope.currentGroup);
+        $scope.currentGroup.lakkautusPvm = null;
+
+        var deferred = $q.defer();
+
+        if ($scope.currentGroup !== null) {
+            $scope.model.save($scope.currentGroup, function(savedGroup) {
+                $scope.currentGroup = savedGroup;
+                $scope.updateUpdateInfo();
+                $scope.currentGroupSelection = {};
+                $scope.currentGroupSelection.selected = savedGroup;
+                $scope.form.$setPristine();
+                deferred.resolve();
+            }, function(error) {
+                loadingService.onErrorHandled(error);
+                $log.warn("Failed to save group: ", $scope.currentGroup);
+                Alert.add("error", $filter('i18n')(error.data.errorKey || 'generic.error'), false);
+                deferred.reject();
+            });
+        }
+        return deferred.promise;
     };
 
     $scope.tallenna = function() {
@@ -199,4 +249,5 @@ app.controller('RyhmienHallintaController', function RyhmienHallintaController($
             });
         }
     });
+
 });
