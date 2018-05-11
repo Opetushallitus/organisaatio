2c56799bdb82e75d0f94f1f4ef70604af25bd47e
Petri Ryhänen <petri.ryhanen@csc.fi>
2018-05-11 11:26:55 +0300

    OH-451: Optimise organisaatio lisatietotyyppi rajoite query by not fetching organisaatio. Add no constraint types to result.

--- organisaatio-service/src/main/java/fi/vm/sade/organisaatio/dao/LisatietoTyyppiDao.java
+++ organisaatio-service/src/main/java/fi/vm/sade/organisaatio/dao/LisatietoTyyppiDao.java
@@ -3,9 +3,8 @@ package fi.vm.sade.organisaatio.dao;
 import fi.vm.sade.generic.dao.JpaDAO;
 import fi.vm.sade.organisaatio.model.Lisatietotyyppi;
 
-import java.util.List;
 import java.util.Set;
 
 public interface LisatietoTyyppiDao extends JpaDAO<Lisatietotyyppi, Long> {
-    Set<String> findValidByOrganisaatiotyyppiAndOppilaitostyyppi(List<String> organisaatiotyyppis, String oppilaitostyyppi);
+    Set<String> findValidByOrganisaatiotyyppiAndOppilaitostyyppi(String organisaatioOid);
 }
