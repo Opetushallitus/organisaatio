2cf169e0769c4f5c866fc626f7264b5d573868bf
Petri Ryhänen <petri.ryhanen@csc.fi>
2018-05-11 11:26:55 +0300

    OH-451: Optimise organisaatio lisatietotyyppi rajoite query by not fetching organisaatio. Add no constraint types to result.

--- organisaatio-service/src/main/java/fi/vm/sade/organisaatio/business/impl/LisatietoServiceImpl.java
+++ organisaatio-service/src/main/java/fi/vm/sade/organisaatio/business/impl/LisatietoServiceImpl.java
@@ -3,13 +3,9 @@ package fi.vm.sade.organisaatio.business.impl;
 import fi.vm.sade.organisaatio.business.LisatietoService;
 import fi.vm.sade.organisaatio.business.OrganisaatioFindBusinessService;
 import fi.vm.sade.organisaatio.dao.LisatietoTyyppiDao;
-import fi.vm.sade.organisaatio.dto.v3.OrganisaatioRDTOV3;
 import fi.vm.sade.organisaatio.model.Lisatietotyyppi;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -34,11 +30,6 @@ public class LisatietoServiceImpl implements LisatietoService {
 
     @Override
     public Set<String> getSallitutByOid(String oid) {
-        List<OrganisaatioRDTOV3> organisaatioRDTOV3s = organisaatioFindBusinessService.findByOids(Collections.singletonList(oid));
-        if (organisaatioRDTOV3s.isEmpty()) {
-            return new HashSet<>();
-        }
-        OrganisaatioRDTOV3 organisaatio = organisaatioRDTOV3s.get(0);
-        return this.lisatietoTyyppiDao.findValidByOrganisaatiotyyppiAndOppilaitostyyppi(organisaatio.getTyypit(), organisaatio.getOppilaitosTyyppiUri());
+        return this.lisatietoTyyppiDao.findValidByOrganisaatiotyyppiAndOppilaitostyyppi(oid);
     }
 }
--- organisaatio-service/src/main/java/fi/vm/sade/organisaatio/dao/impl/LisatietoTyyppiDaoImpl.java
+++ organisaatio-service/src/main/java/fi/vm/sade/organisaatio/dao/impl/LisatietoTyyppiDaoImpl.java
@@ -3,26 +3,21 @@ package fi.vm.sade.organisaatio.dao.impl;
 import com.querydsl.jpa.impl.JPAQuery;
 import fi.vm.sade.generic.dao.AbstractJpaDAOImpl;
 import fi.vm.sade.organisaatio.dao.LisatietoTyyppiDao;
-import fi.vm.sade.organisaatio.model.Lisatietotyyppi;
-import fi.vm.sade.organisaatio.model.QLisatietotyyppi;
-import fi.vm.sade.organisaatio.model.QOppilaitostyyppiRajoite;
-import fi.vm.sade.organisaatio.model.QOrganisaatiotyyppiRajoite;
+import fi.vm.sade.organisaatio.model.*;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static com.querydsl.core.types.ExpressionUtils.anyOf;
-
-
 @Repository
 public class LisatietoTyyppiDaoImpl extends AbstractJpaDAOImpl<Lisatietotyyppi, Long> implements LisatietoTyyppiDao {
 
     @Override
-    public Set<String> findValidByOrganisaatiotyyppiAndOppilaitostyyppi(List<String> organisaatiotyyppis,
-                                                                        String oppilaitostyyppi) {
+    public Set<String> findValidByOrganisaatiotyyppiAndOppilaitostyyppi(String organisaatioOid) {
+        QOrganisaatio organisaatio = QOrganisaatio.organisaatio;
         QLisatietotyyppi lisatietotyyppi = QLisatietotyyppi.lisatietotyyppi;
         QOrganisaatiotyyppiRajoite organisaatiotyyppiRajoite = QOrganisaatiotyyppiRajoite.organisaatiotyyppiRajoite;
         QOppilaitostyyppiRajoite oppilaitostyyppiRajoite = QOppilaitostyyppiRajoite.oppilaitostyyppiRajoite;
@@ -30,22 +25,25 @@ public class LisatietoTyyppiDaoImpl extends AbstractJpaDAOImpl<Lisatietotyyppi, 
         List<String> organisaatioLisatietotyyppiList = new JPAQuery<>(this.getEntityManager())
                 .from(organisaatiotyyppiRajoite)
                 .innerJoin(organisaatiotyyppiRajoite.lisatietotyyppi, lisatietotyyppi)
-                .where(anyOf(
-                        organisaatiotyyppiRajoite.isNull(),
-                        organisaatiotyyppiRajoite.arvo.in(organisaatiotyyppis)
-                        )
-                )
-                .select(lisatietotyyppi.nimi).fetch();
+                .innerJoin(organisaatio).on(organisaatiotyyppiRajoite.arvo.in(organisaatio.tyypit))
+                .select(lisatietotyyppi.nimi)
+                .fetch();
+
         List<String> oppilaitosLisatietotyyppiList = new JPAQuery<>(this.getEntityManager())
                 .from(oppilaitostyyppiRajoite)
                 .innerJoin(oppilaitostyyppiRajoite.lisatietotyyppi, lisatietotyyppi)
-                .where(anyOf(
-                        oppilaitostyyppiRajoite.isNull(),
-                        oppilaitostyyppiRajoite.arvo.eq(oppilaitostyyppi)
-                        )
-                )
-                .select(lisatietotyyppi.nimi).fetch();
-        return Stream.concat(oppilaitosLisatietotyyppiList.stream(), organisaatioLisatietotyyppiList.stream())
+                .innerJoin(organisaatio).on(oppilaitostyyppiRajoite.arvo.eq(organisaatio.oppilaitosTyyppi))
+                .select(lisatietotyyppi.nimi)
+                .fetch();
+
+        List<String> notConstrainedTypesList = new JPAQuery<>(this.getEntityManager())
+                .select(lisatietotyyppi.nimi)
+                .from(lisatietotyyppi)
+                .where(lisatietotyyppi.rajoitteet.isEmpty())
+                .fetch();
+        return Stream.of(oppilaitosLisatietotyyppiList, organisaatioLisatietotyyppiList, notConstrainedTypesList)
+                .flatMap(Collection::stream)
+                .distinct()
                 .collect(Collectors.toSet());
     }
 }
