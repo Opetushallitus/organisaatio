bc237bf79b272c1590b27a95472086d6c32a96f3
Petri Ryhänen <petri.ryhanen@csc.fi>
2018-05-15 09:47:50 +0300

    OH-451: Try updating cxf to version 3.2 to fix returned json string not having quotation marks in body. This didn't fix it so using TEST/PLAIN type instead.

--- organisaatio-service/src/main/java/fi/vm/sade/organisaatio/model/OrganisaatiotyyppiRajoite.java
+++ organisaatio-service/src/main/java/fi/vm/sade/organisaatio/model/OrganisaatiotyyppiRajoite.java
@@ -3,7 +3,9 @@ package fi.vm.sade.organisaatio.model;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
+import static fi.vm.sade.organisaatio.dto.Rajoitetyyppi.CONSTANTS.ORGANISAATIOTYYPPI_CONSTANT;
+
 @Entity
-@DiscriminatorValue("organisaatiotyyppi")
+@DiscriminatorValue(ORGANISAATIOTYYPPI_CONSTANT)
 public class OrganisaatiotyyppiRajoite extends Rajoite {
 }
