9cef61fe4e4db1709ee38bf5812ccc9c861659b7
Petri Ryhänen <petri.ryhanen@csc.fi>
2018-05-24 16:00:30 +0300

    OH-471: Use java configuration on suitable parts.

---
+++ organisaatio-service/src/main/java/fi/vm/sade/organisaatio/config/SpringSessionConnfiguration.java
@@ -0,0 +1,9 @@
+package fi.vm.sade.organisaatio.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
+
+@EnableJdbcHttpSession
+@Configuration
+public class SpringSessionConnfiguration {
+}
