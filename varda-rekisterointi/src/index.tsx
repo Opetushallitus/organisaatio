5720e699e53761cddb93ad79a3a652846a8224a5
Jarkko Pätäri <jarkko.patari@gofore.com>
2019-08-20 12:15:51 +0300

    Add axios configuration

--- varda-rekisterointi/src/index.tsx
+++ varda-rekisterointi/src/index.tsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import axios from 'axios';
+
+axios.interceptors.request.use(config => {
+    config.headers['Caller-Id'] = '1.2.246.562.10.00000000001.varda-rekisterointi';
+    return config;
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
