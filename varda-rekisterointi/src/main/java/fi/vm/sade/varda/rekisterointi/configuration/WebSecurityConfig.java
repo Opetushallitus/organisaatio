f6ea8aaef6dc64361f2b5a66aab2e81a796d5e72
Jarkko Pätäri <jarkko.patari@gofore.com>
2019-08-21 16:03:51 +0300

    Disable security headers

--- varda-rekisterointi/src/main/java/fi/vm/sade/varda/rekisterointi/configuration/WebSecurityConfig.java
+++ varda-rekisterointi/src/main/java/fi/vm/sade/varda/rekisterointi/configuration/WebSecurityConfig.java
@@ -46,6 +46,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.headers().disable().csrf().disable();
         http.authorizeRequests()
                 .antMatchers("/hakija/**").hasRole(ROLE)
                 .and()
