35ce86c1c529a38a611b980740de0cee4e030ebc
Jarkko Pätäri <jarkko.patari@gofore.com>
2019-08-26 13:43:42 +0300

    Add organisaatio endpoint

---
+++ varda-rekisterointi/src/main/java/fi/vm/sade/varda/rekisterointi/controller/hakija/OrganisaatioController.java
@@ -0,0 +1,30 @@
+package fi.vm.sade.varda.rekisterointi.controller.hakija;
+
+import fi.vm.sade.varda.rekisterointi.exception.NotFoundException;
+import fi.vm.sade.varda.rekisterointi.model.OrganisaatioV4Dto;
+import fi.vm.sade.varda.rekisterointi.model.Valtuudet;
+import org.springframework.context.annotation.Scope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/hakija/api/organisaatio")
+@Scope("session")
+public class OrganisaatioController {
+
+    private final Valtuudet valtuudet;
+
+    public OrganisaatioController(Valtuudet valtuudet) {
+        this.valtuudet = valtuudet;
+    }
+
+    @GetMapping
+    public OrganisaatioV4Dto getOrganisaatio() {
+        return Optional.ofNullable(this.valtuudet).flatMap(v -> Optional.ofNullable(v.organisaatio))
+                .orElseThrow(() -> new NotFoundException("Organisaatiota ei löydy istunnosta"));
+    }
+
+}
