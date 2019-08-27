0bbe319a7d01d7c0ddc78e30a28161a843cf2b6f
Jarkko Pätäri <jarkko.patari@gofore.com>
2019-08-27 09:20:06 +0300

    Fix organisaatio mock

--- varda-rekisterointi/src/main/java/fi/vm/sade/varda/rekisterointi/model/OrganisaatioV4Dto.java
+++ varda-rekisterointi/src/main/java/fi/vm/sade/varda/rekisterointi/model/OrganisaatioV4Dto.java
@@ -5,32 +5,30 @@ import fi.vm.sade.suomifi.valtuudet.OrganisationDto;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
+
+import static java.util.Collections.singletonList;
 
 public class OrganisaatioV4Dto extends BaseDto {
 
     public String oid;
 
-    public String parentOid;
     public String ytunnus;
     public Map<String, String> nimi;
     public List<OrganisaatioNimi> nimet;
 
     public LocalDate alkuPvm;
-    public LocalDate lakkautusPvm;
-
-    public String ytjKieli; // koodisto 'kieli'
-    public LocalDate ytjPaivitysPvm;
-
-    public Set<String> kieletUris; // koodisto 'oppilaitoksen opetuskieli'
-    public Set<String> tyypit; // koodisto 'organisaatiotyyppi'
-    public String maaUri; // koodisto 'maat ja valtiot 1'
-    public String kotipaikkaUri; // koodisto 'kunta'
 
     public static OrganisaatioV4Dto of(OrganisationDto dto) {
         OrganisaatioV4Dto organisaatio = new OrganisaatioV4Dto();
         organisaatio.ytunnus = dto.identifier;
         organisaatio.nimi = Map.of("fi", dto.name);
+        OrganisaatioNimi organisaatioNimi = new OrganisaatioNimi();
+        organisaatioNimi.alkuPvm = organisaatio.alkuPvm;
+        organisaatioNimi.nimi = organisaatio.nimi;
+        organisaatio.nimet = singletonList(organisaatioNimi);
+
+        organisaatio.alkuPvm = LocalDate.now();
+
         return organisaatio;
     }
 
