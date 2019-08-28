6e1de756f54ae1ed92ab1360cdfa32c691577936
Jarkko Pätäri <jarkko.patari@gofore.com>
2019-08-28 10:17:01 +0300

    Add react router

--- varda-rekisterointi/src/frontend/App.tsx
+++ varda-rekisterointi/src/frontend/App.tsx
@@ -5,15 +5,18 @@ import Rekisterointi from './hakija/Rekisterointi';
 import { registerLocale } from 'react-datepicker';
 import { fi, sv, enGB } from 'date-fns/locale';
 import { LanguageContext } from './contexts';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 const App: React.FC = () => {
   registerLocale('fi', fi);
   registerLocale('sv', sv);
   registerLocale('en', enGB);
   return (
-    <LanguageContext.Provider value="fi">
-      <Rekisterointi />
-    </LanguageContext.Provider>
+    <Router basename="/varda-rekisterointi">
+      <LanguageContext.Provider value="fi">
+        <Route path="/hakija" exact component={Rekisterointi} />
+      </LanguageContext.Provider>
+    </Router>
   );
 }
 
