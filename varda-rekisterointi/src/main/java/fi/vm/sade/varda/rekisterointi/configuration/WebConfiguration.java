d2e0aa40f0c40db30824e2d707fc94def1928248
Jarkko Pätäri <jarkko.patari@gofore.com>
2019-08-28 08:32:53 +0300

    Serve ui with spring

---
+++ varda-rekisterointi/src/main/java/fi/vm/sade/varda/rekisterointi/configuration/WebConfiguration.java
@@ -0,0 +1,20 @@
+package fi.vm.sade.varda.rekisterointi.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/{spring:\\w+}")
+                .setViewName("forward:/index.html");
+        registry.addViewController("/**/{spring:\\w+}")
+                .setViewName("forward:/index.html");
+        registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css)$}")
+                .setViewName("forward:/index.html");
+    }
+
+}
