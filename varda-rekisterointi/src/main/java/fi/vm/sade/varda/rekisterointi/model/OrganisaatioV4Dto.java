bc4194642d371dc78a3997877be393f6e2ecfb65
Jarkko Pätäri <jarkko.patari@gofore.com>
2019-08-28 07:54:40 +0300

    Fix organisaatio mock

--- varda-rekisterointi/src/main/java/fi/vm/sade/varda/rekisterointi/model/OrganisaatioV4Dto.java
+++ varda-rekisterointi/src/main/java/fi/vm/sade/varda/rekisterointi/model/OrganisaatioV4Dto.java
@@ -22,13 +22,11 @@ public class OrganisaatioV4Dto extends BaseDto {
         OrganisaatioV4Dto organisaatio = new OrganisaatioV4Dto();
         organisaatio.ytunnus = dto.identifier;
         organisaatio.nimi = Map.of("fi", dto.name);
+        organisaatio.alkuPvm = LocalDate.now();
         OrganisaatioNimi organisaatioNimi = new OrganisaatioNimi();
         organisaatioNimi.alkuPvm = organisaatio.alkuPvm;
         organisaatioNimi.nimi = organisaatio.nimi;
         organisaatio.nimet = singletonList(organisaatioNimi);
-
-        organisaatio.alkuPvm = LocalDate.now();
-
         return organisaatio;
     }
 
