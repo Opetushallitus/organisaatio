02c1a152503da555b46ec0b1f74b02ba0077b7a2
Jarkko Pätäri <jarkko.patari@gofore.com>
2019-09-02 15:13:48 +0300

    Add image to final page

--- varda-rekisterointi/src/frontend/hakija/RekisterointiValmis.tsx
+++ varda-rekisterointi/src/frontend/hakija/RekisterointiValmis.tsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { LanguageContext } from '../contexts';
+import { ReactComponent as Image } from './hakemus odottaa hyväksyntää.svg';
 
 export default function RekisterointiValmis() {
     const { i18n } = useContext(LanguageContext);
@@ -10,6 +11,7 @@ export default function RekisterointiValmis() {
             </fieldset>
             <div>{i18n.translate('REKISTEROINNIN_KASITTELY_KUVAUS')}</div>
             <div>{i18n.translate('REKISTEROINNIN_KASITTELY_OHJE')}</div>
+            <div className="center"><Image /></div>
         </form>
     );
 }
