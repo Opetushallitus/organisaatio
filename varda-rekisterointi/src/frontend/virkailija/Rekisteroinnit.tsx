73e052323330e469d623bbaf23d0228b4406625f
Jani Kaarela <jani.kaarela@gmail.com>
2019-10-02 14:04:45 +0300

    Refaktorointia, rekisteröinnin tila.

--- varda-rekisterointi/src/frontend/virkailija/Rekisteroinnit.tsx
+++ varda-rekisterointi/src/frontend/virkailija/Rekisteroinnit.tsx
@@ -1,50 +1,23 @@
-import React, {useContext, useEffect, useState} from "react";
-import {LanguageContext} from '../contexts';
-import RekisterointiLista from './RekisterointiLista';
-import Axios from "axios";
-import {Rekisterointihakemus} from "./rekisterointihakemus";
-import Spinner from "../Spinner";
+import React, {useContext} from "react";
+import {LanguageContext} from "../contexts";
+import RekisterointiLista from "./RekisterointiLista";
+import Box from "@opetushallitus/virkailija-ui-components/Box";
+import createTheme from "@opetushallitus/virkailija-ui-components/createTheme";
+import {ThemeProvider} from "styled-components";
 
-const rekisteroinnitUrl = "/varda-rekisterointi/virkailija/api/rekisteroinnit";
-const tyhjaLista: Rekisterointihakemus[] = [];
+const theme = createTheme();
 
 export default function Rekisteroinnit() {
     const { i18n } = useContext(LanguageContext);
-    const [rekisteroinnit, asetaRekisteroinnit] = useState(tyhjaLista);
-    const [latausKesken, asetaLatausKesken] = useState(true);
-    const [latausVirhe, asetaLatausVirhe] = useState(false);
 
-    useEffect(() => {
-        async function lataa() {
-            try {
-                asetaLatausKesken(true);
-                asetaLatausVirhe(false);
-                const response = await Axios.get(rekisteroinnitUrl);
-                const data = response.data;
-                asetaRekisteroinnit(data);
-            } catch (e) {
-                asetaLatausVirhe(true);
-                console.error(e);
-            } finally {
-                asetaLatausKesken(false);
-            }
-        }
-        lataa();
-    }, []);
-
-    if (latausKesken) {
-        return <Spinner/>;
-    }
-
-    if (latausVirhe) {
-        return <div className="virhe">{i18n.translate('REKISTEROINNIT_LATAUSVIRHE')}</div>
-    }
 
     return (
-        <div className="varda-rekisteroinnit">
-            <h2>{i18n.translate('REKISTEROINNIT_OTSIKKO')}</h2>
-            <p>{i18n.translate('REKISTEROINNIT_KUVAUS')}</p>
-            <RekisterointiLista rekisteroinnit={rekisteroinnit}/>
-        </div>
+        <ThemeProvider theme={theme}>
+            <Box className="varda-rekisteroinnit">
+                <h2>{i18n.translate('REKISTEROINNIT_OTSIKKO')}</h2>
+                <p>{i18n.translate('REKISTEROINNIT_KUVAUS')}</p>
+                <RekisterointiLista />
+            </Box>
+        </ThemeProvider>
     );
 }
--- varda-rekisterointi/src/frontend/virkailija/RekisterointiLista.tsx
+++ varda-rekisterointi/src/frontend/virkailija/RekisterointiLista.tsx
@@ -1,15 +1,26 @@
-import React, {useContext} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import {format, parseISO} from 'date-fns';
+import Axios from "axios";
 import {LanguageContext} from '../contexts';
-import {Rekisterointihakemus} from "./rekisterointihakemus";
+import {Rekisterointihakemus, Tila} from "./rekisterointihakemus";
 import {Lista} from "../Lista";
 
+import Box from "@opetushallitus/virkailija-ui-components/Box";
+import Spin from "@opetushallitus/virkailija-ui-components/Spin";
+
+const rekisteroinnitUrl = "/varda-rekisterointi/virkailija/api/rekisteroinnit";
+const tyhjaLista: Rekisterointihakemus[] = [];
+
 type Props = {
-    rekisteroinnit: Rekisterointihakemus[];
+    tila?: Tila
 }
 
-export default function RekisterointiLista({ rekisteroinnit } : Props) {
+export default function RekisterointiLista({ tila = Tila.KASITTELYSSA } : Props) {
     const { i18n } = useContext(LanguageContext);
+    const [rekisteroinnit, asetaRekisteroinnit] = useState(tyhjaLista);
+    const [latausKesken, asetaLatausKesken] = useState(true);
+    const [latausVirhe, asetaLatausVirhe] = useState(false);
+    const [hakutermi, asetaHakutermi] = useState("");
     const saapumisAikaFormat = 'd.M.y HH:mm';
     const otsikot = [
         i18n.translate("ORGANISAATION_NIMI"),
@@ -26,13 +37,39 @@ export default function RekisterointiLista({ rekisteroinnit } : Props) {
             { data: format(parseISO(rekisterointi.vastaanotettu), saapumisAikaFormat) }
         ];
     };
+
+    useEffect(() => {
+        async function lataa() {
+            try {
+                asetaLatausKesken(true);
+                asetaLatausVirhe(false);
+                const response = await Axios.get(rekisteroinnitUrl, { params: { tila }});
+                asetaRekisteroinnit(response.data);
+            } catch (e) {
+                asetaLatausVirhe(true);
+                console.error(e);
+            } finally {
+                asetaLatausKesken(false);
+            }
+        }
+        lataa();
+    }, [tila]);
+
+    if (latausKesken) {
+        return <Spin />;
+    }
+
+    if (latausVirhe) {
+        return <div className="virhe">{i18n.translate('REKISTEROINNIT_LATAUSVIRHE')}</div>
+    }
+
     return (
-        <div className="varda-rekisterointi-lista">
+        <Box className="varda-rekisterointi-lista">
             <Lista
                 otsikot={otsikot}
                 rivit={rekisteroinnit}
                 tunnisteGeneraattori={tunnisteGeneraattori}
                 sarakeGeneraattori={sarakeGeneraattori}/>
-        </div>
+        </Box>
     )
 }
