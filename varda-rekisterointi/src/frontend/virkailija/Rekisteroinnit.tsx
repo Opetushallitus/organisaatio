201ef77e6a47eed01a45f431c24120eb0e659613
Jani Kaarela <jani.kaarela@gmail.com>
2019-10-10 15:22:56 +0300

    Kesken: rekisteröintihakemusten filtteröinti.

--- varda-rekisterointi/src/frontend/virkailija/Rekisteroinnit.tsx
+++ varda-rekisterointi/src/frontend/virkailija/Rekisteroinnit.tsx
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from "react";
+import React, {ChangeEvent, useContext, useState} from "react";
 import {LanguageContext} from "../contexts";
 import {ConfigurationContext} from "../contexts";
 import RekisterointiLista from "./RekisterointiLista";
@@ -9,17 +9,23 @@ import {ThemeProvider} from "styled-components";
 import {Tila} from "./rekisterointihakemus";
 import Tabs from "@opetushallitus/virkailija-ui-components/Tabs";
 import Tab from "@opetushallitus/virkailija-ui-components/Tab";
+import Input from "@opetushallitus/virkailija-ui-components/Input";
+import FilterVariantIcon from "mdi-react/FilterVariantIcon";
 
 const theme = createTheme();
 
 export default function Rekisteroinnit() {
     const { i18n } = useContext(LanguageContext);
     const configuration = useContext(ConfigurationContext);
-    //const [hakutermi, asetaHakutermi] = useState("");
+    const [hakutermi, asetaHakutermi] = useState("");
     const [tila, asetaTila] = useState(Tila.KASITTELYSSA);
 
     function vaihdaTila(uusiTila: string) {
         asetaTila(uusiTila as Tila);
+    }
+
+    function vaihdaHakuTermi(event: ChangeEvent<HTMLInputElement>) {
+        asetaHakutermi(event.target.value);
     }
 
     return (
@@ -29,6 +35,9 @@ export default function Rekisteroinnit() {
                 <Box className="varda-rekisteroinnit">
                     <h2>{i18n.translate('REKISTEROINNIT_OTSIKKO')}</h2>
                     <p>{i18n.translate('REKISTEROINNIT_KUVAUS')}</p>
+                    <Input type="text"
+                           placeholder={i18n.translate('REKISTEROINNIT_SUODATA')} value={hakutermi}
+                           prefix={<FilterVariantIcon />} onChange={vaihdaHakuTermi} />
                     <Tabs value={tila} onChange={vaihdaTila}>
                         <Tab value={Tila.KASITTELYSSA}>{ i18n.translate(`REKISTEROINNIT_TILA_KASITTELYSSA`) }</Tab>
                         <Tab value={Tila.HYVAKSYTTY}>{ i18n.translate(`REKISTEROINNIT_TILA_HYVAKSYTTY`) }</Tab>
