596ed6be5f9da6697a3a90a90157c4d1a1cf036d
Jarkko Pätäri <jarkko.patari@gofore.com>
2019-11-08 11:01:20 +0200

    Add 404 error route

--- varda-rekisterointi/src/frontend/App.tsx
+++ varda-rekisterointi/src/frontend/App.tsx
@@ -6,11 +6,12 @@ import Rekisteroinnit from "./virkailija/Rekisteroinnit";
 import { registerLocale } from 'react-datepicker';
 import { fi, sv, enGB } from 'date-fns/locale';
 import { LanguageContext, I18nImpl } from './contexts';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Language, Lokalisointi } from './types';
 import useAxios from 'axios-hooks';
 import Spinner from './Spinner';
 import RekisterointiVirkailija from './virkailija/RekisterointiVirkailija';
+import ErrorPage from './ErrorPage';
 
 const App: React.FC = () => {
   registerLocale('fi', fi);
@@ -21,12 +22,18 @@ const App: React.FC = () => {
   if (loading) {
     return <Spinner />;
   }
+  const i18n = new I18nImpl(data[language]);
   return (
     <Router basename="/varda-rekisterointi">
-      <LanguageContext.Provider value={{ language: language, setLanguage: setLanguage, i18n: new I18nImpl(data[language]) }}>
-        <Route path="/hakija" exact component={RekisterointiHakija} />
-        <Route path="/virkailija" exact component={Rekisteroinnit} />
-        <Route path="/virkailija/rekisterointi/luonti/:ytunnus" exact component={RekisterointiVirkailija} />
+      <LanguageContext.Provider value={{ language: language, setLanguage: setLanguage, i18n: i18n }}>
+        <Switch>
+          <Route path="/hakija" exact component={RekisterointiHakija} />
+          <Route path="/virkailija" exact component={Rekisteroinnit} />
+          <Route path="/virkailija/rekisterointi/luonti/:ytunnus" exact component={RekisterointiVirkailija} />
+          <Route path="*">
+            <ErrorPage>{i18n.translate('ERROR_404')}</ErrorPage>
+          </Route>
+        </Switch>
       </LanguageContext.Provider>
     </Router>
   );
