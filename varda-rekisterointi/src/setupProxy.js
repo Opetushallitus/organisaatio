423bbd54c128544bba4eaa8a1db090e3a22ba420
Jarkko Pätäri <jarkko.patari@gofore.com>
2019-11-12 10:25:46 +0200

    Fix j_spring_cas_security_check redirect in dev

--- varda-rekisterointi/src/setupProxy.js
+++ varda-rekisterointi/src/setupProxy.js
@@ -1,10 +1,10 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
-  app.use(proxy('/varda-rekisterointi/api', { target: 'http://localhost:8081/' }));
-  app.use(proxy('/varda-rekisterointi/hakija/login', { target: 'http://localhost:8081/' }));
-  app.use(proxy('/varda-rekisterointi/hakija/valtuudet', { target: 'http://localhost:8081/' }));
-  app.use(proxy('/varda-rekisterointi/hakija/api', { target: 'http://localhost:8081/' }));
-  app.use(proxy('/varda-rekisterointi/virkailija/api', { target: 'http://localhost:8081/' }));
-  app.use(proxy('/varda-rekisterointi/virkailija/j_spring_cas_security_check', { target: 'http://localhost:8081/' }));
+  app.use(proxy('/varda-rekisterointi/api', { target: 'http://localhost:8081/', xfwd: true }));
+  app.use(proxy('/varda-rekisterointi/hakija/login', { target: 'http://localhost:8081/', xfwd: true }));
+  app.use(proxy('/varda-rekisterointi/hakija/valtuudet', { target: 'http://localhost:8081/', xfwd: true }));
+  app.use(proxy('/varda-rekisterointi/hakija/api', { target: 'http://localhost:8081/', xfwd: true }));
+  app.use(proxy('/varda-rekisterointi/virkailija/api', { target: 'http://localhost:8081/', xfwd: true }));
+  app.use(proxy('/varda-rekisterointi/virkailija/j_spring_cas_security_check', { target: 'http://localhost:8081/', xfwd: true }));
 };
