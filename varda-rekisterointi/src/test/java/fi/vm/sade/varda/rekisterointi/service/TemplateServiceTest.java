584e00871b52c76b75d260197d4d73df8b86491e
Jarkko Pätäri <jarkko.patari@gofore.com>
2019-12-13 12:12:23 +0200

    Add email translations

--- varda-rekisterointi/src/test/java/fi/vm/sade/varda/rekisterointi/service/TemplateServiceTest.java
+++ varda-rekisterointi/src/test/java/fi/vm/sade/varda/rekisterointi/service/TemplateServiceTest.java
@@ -12,12 +12,19 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.rules.SpringClassRule;
 import org.springframework.test.context.junit4.rules.SpringMethodRule;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 import java.util.Locale;
+
+import static java.util.stream.Collectors.toList;
 
 @RunWith(Parameterized.class)
 @SpringBootTest
 @ActiveProfiles("test")
 public class TemplateServiceTest {
+
+    private static final Collection<String> LANGUAGES = List.of("fi", "sv");
 
     @ClassRule
     public static final SpringClassRule SPRING_CLASS_RULE = new SpringClassRule();
@@ -28,20 +35,24 @@ public class TemplateServiceTest {
     @Autowired
     private TemplateService templateService;
 
-    @Parameterized.Parameters(name = "{0}")
-    public static Template[] parameters() {
-        return Template.values();
+    @Parameterized.Parameters(name = "{0}:{1}")
+    public static Collection<Object[]> parameters() {
+        return Arrays.stream(Template.values())
+                .flatMap(template -> LANGUAGES.stream().map(language -> new Object[]{ template, language }))
+                .collect(toList());
     }
 
     private final Template template;
+    private final String language;
 
-    public TemplateServiceTest(Template template) {
+    public TemplateServiceTest(Template template, String language) {
         this.template = template;
+        this.language = language;
     }
 
     @Test
     public void renderWorks() {
-        String content = templateService.getContent(template, new Locale("fi"));
+        String content = templateService.getContent(template, new Locale(language));
         System.out.println(content);
     }
 
