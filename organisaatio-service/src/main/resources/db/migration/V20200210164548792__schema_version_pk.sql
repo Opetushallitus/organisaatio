9bfeb930380941d403e3195fe43c59458e176264
Jani Kaarela <jani.kaarela@gmail.com>
2020-02-11 08:39:36 +0200

    Kommentoitu migraatiokomennot.
    Koska Flyway käyttää schema_version -taulua, sen käpisteleminen
    Flywaylla näyttäisi aiheuttavan deadlockin...

--- organisaatio-service/src/main/resources/db/migration/V20200210164548792__schema_version_pk.sql
+++ organisaatio-service/src/main/resources/db/migration/V20200210164548792__schema_version_pk.sql
@@ -1,3 +1,4 @@
 -- varmistaa, että PK on olemassa, jotta päivitys 4.x:ään onnistuu
-ALTER TABLE schema_version DROP CONSTRAINT IF EXISTS schema_version_pk;
-ALTER TABLE schema_version ADD CONSTRAINT schema_version_pk PRIMARY KEY (installed_rank);
+-- ajettava manuaalisesti ennen deplua, koska Flyway käyttää taulua ja aiheuttaa deadlockin
+-- ALTER TABLE schema_version DROP CONSTRAINT IF EXISTS schema_version_pk;
+-- ALTER TABLE schema_version ADD CONSTRAINT schema_version_pk PRIMARY KEY (installed_rank);
