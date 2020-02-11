5b75fa3ee0bdfe0496d1950b33be6e200bdf1541
Jani Kaarela <jani.kaarela@gmail.com>
2020-02-11 10:12:16 +0200

    Create extension omaan migraatiotiedostoonsa.

---
+++ organisaatio-service/src/main/resources/db/migration/V20200211095318104__create_trgm_extension.sql
@@ -0,0 +1,1 @@
+CREATE EXTENSION IF NOT EXISTS pg_trgm; -- infratiimin pitää ajaa tämä käsin ennen asennusta!
--- organisaatio-service/src/main/resources/db/migration/V20200211095318105__organisaation_parentoid_indeksi.sql
+++ organisaatio-service/src/main/resources/db/migration/V20200211095318105__organisaation_parentoid_indeksi.sql
@@ -1,3 +1,2 @@
-CREATE EXTENSION IF NOT EXISTS pg_trgm; -- infratiimin pitää ajaa tämä käsin ennen asennusta!
 DROP INDEX CONCURRENTLY IF EXISTS organisaatio_parentoidpath_idx;
 CREATE INDEX CONCURRENTLY organisaatio_parentoidpath_idx ON organisaatio USING gin (parentoidpath gin_trgm_ops);
