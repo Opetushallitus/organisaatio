e41019d3e79869a995b0f1f994dbcfff362979af
Jani Kaarela <jani.kaarela@gmail.com>
2020-02-11 13:27:44 +0200

    Ei dropata vanhaa indeksiä. Lisätään uusi.
    Uuden indeksin lisääminen olisi parasta tehdä manuaalisesti
    sopivana ajankohtana.

--- organisaatio-service/src/main/resources/db/migration/V20200211095318105__organisaation_parentoid_indeksi.sql
+++ organisaatio-service/src/main/resources/db/migration/V20200211095318105__organisaation_parentoid_indeksi.sql
@@ -1,2 +1,3 @@
-DROP INDEX CONCURRENTLY IF EXISTS organisaatio_parentoidpath_idx;
-CREATE INDEX CONCURRENTLY organisaatio_parentoidpath_idx ON organisaatio USING gin (parentoidpath gin_trgm_ops);
+-- ajetaan mieluiten manuaalisesti sopivana ajankohtana concurrently:
+-- CREATE INDEX CONCURRENTLY organisaatio_parentoidpath_trgm_idx ON organisaatio USING gin (parentoidpath gin_trgm_ops);
+CREATE INDEX IF NOT EXISTS organisaatio_parentoidpath_trgm_idx ON organisaatio USING gin (parentoidpath gin_trgm_ops);
