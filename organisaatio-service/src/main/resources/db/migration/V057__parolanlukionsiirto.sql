29e8e3aa25a323c704bc563c6909335a1d7be288
Jani Kaarela <jani.kaarela@gmail.com>
2020-05-19 14:52:02 +0300

    Ajetaan kantamigraatiot integraatiotestivaiheessa.
    Muutettu migraatiota V057 siten, ettei se feilaa, mikäli
    kannassa ei ole organisaatioita annetuilla id:illä.

--- organisaatio-service/src/main/resources/db/migration/V057__parolanlukionsiirto.sql
+++ organisaatio-service/src/main/resources/db/migration/V057__parolanlukionsiirto.sql
@@ -6,8 +6,10 @@
 update organisaatiosuhde set loppupvm=timestamp '2014-07-31 23:59:59.999',paivityspvm=current_date where child_id=12570 and parent_id=790;
 
 -- Parolan lukio (oppilaitos) on siirtynyt koulutuskuntayhtymä Tavastian alle
+-- insert into / select, jotta insert ei hajoa mikäli child tai parent ei ole olemassa
+insert into organisaatiosuhde (id, version, alkupvm, suhdetyyppi, child_id, parent_id, paivityspvm, paivittaja)
+ select nextval('public.hibernate_sequence'), 0, timestamp '2014-08-01 00:00:00.000', 'HISTORIA', child_id, parent_id, current_date, 'oph' from organisaatiosuhde where child_id = 12570 and parent_id = 3226;
 delete from organisaatiosuhde where child_id=12570 and parent_id=3226 and alkupvm=timestamp '2014-08-01 00:00:00.000';
-insert into organisaatiosuhde (id, version, alkupvm, suhdetyyppi, child_id, parent_id, paivityspvm, paivittaja) values (nextval('public.hibernate_sequence'), 0, timestamp '2014-08-01 00:00:00.000', 'HISTORIA', 12570, 3226, current_date, 'oph' );
 
 -- Päivitetään parolan lukion (oppilaitos) parent-tiedot
 update organisaatio set parentidpath='|0|3226|', parentoidpath='|1.2.246.562.10.00000000001|1.2.246.562.10.83097956748|' where id=12570 and parentidpath='|0|790|';
