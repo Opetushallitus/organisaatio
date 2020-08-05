b196d640001e51eb77527ddc60a4a948a3a8939c
mkamka <miikka.kajanne@wunderdog.fi>
2020-08-05 09:20:52 +0300

    poistettu build kansiot

--- organisaatio-service/build/resources/test/data/root_organisaatio_data.sql
+++
@@ -1,7 +0,0 @@
-insert into monikielinenteksti (id, version) values (0, 1);
-insert into monikielinenteksti_values (id, value, key) values (0, 'Opetushallitus', 'fi');
-insert into monikielinenteksti_values (id, value, key) values (0, 'Utbildningsstyrelsen', 'sv');
-insert into monikielinenteksti_values (id, value, key) values (0, 'The Finnish National Board of Education', 'en');
-insert into organisaatio (id, version, alkuPvm, nimihaku, oid, organisaatioPoistettu, nimi_mkt)
-    values (0, 0, '1970-01-01', 'oph,,', '1.2.246.562.24.00000000001', FALSE, 0);
-insert into organisaatio_tyypit (organisaatio_id, tyypit) values (0, 'Muu organisaatio');
