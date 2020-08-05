0971359a7fd9cbc56333d80e1199c26290954b08
mkamka <miikka.kajanne@wunderdog.fi>
2020-08-05 09:46:25 +0300

    ui:n polut vastaamaan pilven routausta

--- organisaatio-ui/src/setupProxy.js
+++ organisaatio-ui/src/setupProxy.js
@@ -3,7 +3,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:8180',
+      target: 'http://localhost:8080',
       changeOrigin: true,
       pathRewrite: {'^/api' : ''},
     })
