0eeb4b8d2ab50ea20f6de911ca912662faf70afc
Mikael Bärlund <mikael.barlund@wunderdog.fi>
2021-09-14 15:27:09 +0300

    revert to addDefault

--- organisaatio-service/src/main/java/fi/vm/sade/organisaatio/config/UrlConfiguration.java
+++ organisaatio-service/src/main/java/fi/vm/sade/organisaatio/config/UrlConfiguration.java
@@ -13,18 +13,18 @@ public class UrlConfiguration  extends OphProperties {
         addFiles("/organisaatio-service-oph.properties");
         this.addOverride("host.virkailija", environment.getRequiredProperty("host.virkailija"));
         this.addOverride("url-virkailija", environment.getRequiredProperty("url-virkailija"));
-        this.addOverride("host.alb", environment.getRequiredProperty("host.alb"));
-        this.addOverride("organisaatio.service.username", environment.getRequiredProperty("organisaatio.service.username"));
-        this.addOverride("organisaatio.service.password", environment.getRequiredProperty("organisaatio.service.password"));
-        this.addOverride("organisaatio.service.username.to.koodisto", environment.getRequiredProperty("organisaatio.service.username.to.koodisto"));
-        this.addOverride("organisaatio.service.password.to.koodisto", environment.getRequiredProperty("organisaatio.service.password.to.koodisto"));
-        this.addOverride("organisaatio.service.username.to.viestinta", environment.getRequiredProperty("organisaatio.service.password.to.viestinta"));
-        this.addOverride("organisaatio.service.password.to.viestinta", environment.getRequiredProperty("organisaatio.service.password.to.viestinta"));
-        this.addOverride("organisaatio.ui.url", environment.getRequiredProperty("organisaatio.ui.url"));
-        this.addOverride("organisaatio.ui.ilmoitukset.url", environment.getRequiredProperty("organisaatio.ui.ilmoitukset.url"));
-        this.addOverride("port.koodisto-service", environment.getRequiredProperty("port.koodisto-service"));
-        this.addOverride("port.tarjonta-service", environment.getRequiredProperty("port.tarjonta-service"));
-        this.addOverride("port.organisaatio-service", environment.getRequiredProperty("port.organisaatio-service"));
+        this.addDefault("host.alb", environment.getRequiredProperty("host.alb"));
+        this.addDefault("organisaatio.service.username", environment.getRequiredProperty("organisaatio.service.username"));
+        this.addDefault("organisaatio.service.password", environment.getRequiredProperty("organisaatio.service.password"));
+        this.addDefault("organisaatio.service.username.to.koodisto", environment.getRequiredProperty("organisaatio.service.username.to.koodisto"));
+        this.addDefault("organisaatio.service.password.to.koodisto", environment.getRequiredProperty("organisaatio.service.password.to.koodisto"));
+        this.addDefault("organisaatio.service.username.to.viestinta", environment.getRequiredProperty("organisaatio.service.password.to.viestinta"));
+        this.addDefault("organisaatio.service.password.to.viestinta", environment.getRequiredProperty("organisaatio.service.password.to.viestinta"));
+        this.addDefault("organisaatio.ui.url", environment.getRequiredProperty("organisaatio.ui.url"));
+        this.addDefault("organisaatio.ui.ilmoitukset.url", environment.getRequiredProperty("organisaatio.ui.ilmoitukset.url"));
+        this.addDefault("port.koodisto-service", environment.getRequiredProperty("port.koodisto-service"));
+        this.addDefault("port.tarjonta-service", environment.getRequiredProperty("port.tarjonta-service"));
+        this.addDefault("port.organisaatio-service", environment.getRequiredProperty("port.organisaatio-service"));
         this.frontProperties.setProperty("urlVirkailija", this.require("url-virkailija"));
     }
 }
