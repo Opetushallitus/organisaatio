60bf07436d89c50bc833ec8e65d7cf1088d3a1ce
Mikael Bärlund <mikael.barlund@wunderdog.fi>
2021-09-14 14:22:07 +0300

    fix typo in porp name

--- organisaatio-ui/src/api/useCAS.ts
+++ organisaatio-ui/src/api/useCAS.ts
@@ -3,7 +3,7 @@ import { urls } from 'oph-urls-js';
 import { CASMe } from '../types/apiTypes';
 
 export function useCASLanguage() {
-    const virkailija = urls.url('url-virkailija');
+    const virkailija = urls.url('urlVirkailija');
     const url = `${virkailija}/kayttooikeus-service/cas/me`;
     const [{ data, loading, error }] = useAxios<CASMe>(url);
     return { data: data && data.lang, loading, error };
