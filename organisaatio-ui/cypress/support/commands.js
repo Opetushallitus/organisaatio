3615da7bc1f8580ae89ff5643a306575fb716dc7
Mikael Bärlund <mikael.barlund@wunderdog.fi>
2021-09-27 15:29:49 +0300

    scroll into view in cypress

--- organisaatio-ui/cypress/support/commands.js
+++ organisaatio-ui/cypress/support/commands.js
@@ -28,20 +28,20 @@ Cypress.Commands.add('inputByName', (name, value) => {
 });
 
 Cypress.Commands.add('clickButton', (contains) => {
-    return cy.get('button').contains(contains).click();
+    return cy.get('button').contains(contains).scrollIntoView().click();
 });
 
 Cypress.Commands.add('clickRadioOrCheckbox', (contains) => {
-    return cy.get('div').contains(contains).click();
+    return cy.get('div').contains(contains).scrollIntoView().click();
 });
 
 Cypress.Commands.add('clickAccordion', (contains) => {
-    return cy.get('span').contains(contains).click();
+    return cy.get('span').contains(contains).scrollIntoView().click();
 });
 
 Cypress.Commands.add('selectFromList', (list, conatins) => {
-    cy.get('label').contains(list).parent().find('svg').last().click();
-    return cy.get('div').contains(conatins).click();
+    cy.get('label').contains(list).parent().find('svg').last().scrollIntoView().click();
+    return cy.get('div').contains(conatins).scrollIntoView().click();
 });
 
 Cypress.Commands.add('enterDate', (label, date) => {
@@ -86,7 +86,7 @@ Cypress.Commands.add('enterAllYhteystiedot', (prefix) => {
 
 Cypress.Commands.add('clickSaveButton', () => {
     cy.intercept('POST', '/organisaatio/organisaatio/v4').as('saveOrg');
-    cy.get('button').contains('TALLENNA').click();
+    cy.get('button').contains('TALLENNA').scrollIntoView().click();
     return cy.wait(['@saveOrg'], { timeout: 10000 });
 });
 
