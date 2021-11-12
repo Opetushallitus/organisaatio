a94a12231a9d2c202c9d3eaa2a24dea42b679014
Mikael Bärlund <mikael.barlund@wunderdog.fi>
2021-11-12 15:26:58 +0200

    remove j_spring_cas forward

--- organisaatio-service/src/main/java/fi/vm/sade/organisaatio/config/WebConfiguration.java
+++ organisaatio-service/src/main/java/fi/vm/sade/organisaatio/config/WebConfiguration.java
@@ -15,8 +15,6 @@ public class WebConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController(String.format("%s/j_spring_cas_security_check{anywhere:.*}", uiPath))
-                .setViewName("forward:/j_spring_cas_security_check{anywhere}");
         registry.addViewController(String.format("%s/actuator/health", uiPath))
                 .setViewName("forward:/actuator/health");
         registry.addViewController(String.format("%s/actuator/health/", uiPath))
