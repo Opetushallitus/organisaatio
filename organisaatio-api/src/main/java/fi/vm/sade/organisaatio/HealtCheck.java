5a16a4a8f5e480b6000f098ce3c75fba16c99877
Mikael Bärlund <mikael.barlund@wunderdog.fi>
2021-11-17 12:30:10 +0200

    add endpoints for use with old ui

---
+++ organisaatio-api/src/main/java/fi/vm/sade/organisaatio/HealtCheck.java
@@ -0,0 +1,10 @@
+package fi.vm.sade.organisaatio;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+
+public interface HealtCheck {
+    @GetMapping(path = "/", produces = MediaType.TEXT_PLAIN_VALUE)
+    String hello();
+
+}
