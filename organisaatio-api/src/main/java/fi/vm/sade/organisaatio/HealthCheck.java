96f599201630439d60eca1021feaa882edc73c61
Mikael Bärlund <mikael.barlund@wunderdog.fi>
2021-11-17 12:48:56 +0200

    fix typo

--- organisaatio-api/src/main/java/fi/vm/sade/organisaatio/HealthCheck.java
+++ organisaatio-api/src/main/java/fi/vm/sade/organisaatio/HealthCheck.java
@@ -3,7 +3,7 @@ package fi.vm.sade.organisaatio;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 
-public interface HealtCheck {
+public interface HealthCheck {
     @GetMapping(path = "/", produces = MediaType.TEXT_PLAIN_VALUE)
     String hello();
 
--- organisaatio-service/src/main/java/fi/vm/sade/organisaatio/resource/HealthCheckImpl.java
+++ organisaatio-service/src/main/java/fi/vm/sade/organisaatio/resource/HealthCheckImpl.java
@@ -1,12 +1,12 @@
 package fi.vm.sade.organisaatio.resource;
 
-import fi.vm.sade.organisaatio.HealtCheck;
+import fi.vm.sade.organisaatio.HealthCheck;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("${server.service.context-path}/")
-public class HealtCheckImpl implements HealtCheck {
+public class HealthCheckImpl implements HealthCheck {
 
     @Override
     public String hello() {
