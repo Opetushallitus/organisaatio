c258075eed6cb06036769637b9a56aaf1dd2b26a
Mikael Bärlund <mikael.barlund@wunderdog.fi>
2021-11-19 16:00:36 +0200

    add types to component

--- organisaatio-ui/src/components/DatePickerController/DatePickerController.tsx
+++ organisaatio-ui/src/components/DatePickerController/DatePickerController.tsx
@@ -1,9 +1,14 @@
-import { Controller } from 'react-hook-form';
+import { Control, Controller } from 'react-hook-form';
 import DatePickerInput from '@opetushallitus/virkailija-ui-components/DatePickerInput';
 import moment from 'moment';
 import * as React from 'react';
+import { Perustiedot } from '../../types/types';
 
-export default function DatePickerController(props: { form; validationErrors; name }) {
+export default function DatePickerController(props: {
+    form: Control<Perustiedot> | undefined;
+    validationErrors: { [x: string]: any };
+    name: 'alkuPvm';
+}) {
     return (
         <Controller
             control={props.form}
