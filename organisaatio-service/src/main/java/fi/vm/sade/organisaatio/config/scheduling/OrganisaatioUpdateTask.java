8fd69a7ab770905e2162f4da4309a301a42d7c0b
Mikael Bärlund <mikael.barlund@wunderdog.fi>
2021-11-30 15:16:18 +0200

    donnt run scheduled tasks in dev mode

--- organisaatio-service/src/main/java/fi/vm/sade/organisaatio/config/scheduling/OrganisaatioUpdateTask.java
+++ organisaatio-service/src/main/java/fi/vm/sade/organisaatio/config/scheduling/OrganisaatioUpdateTask.java
@@ -10,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.support.CronSequenceGenerator;
 import org.springframework.stereotype.Component;
 
@@ -20,6 +21,7 @@ import java.util.Date;
 /**
  * Laukaisee organisaatioiden ajastetut päivitys operaatiot
  */
+@Profile("!dev")
 @Component
 public class OrganisaatioUpdateTask extends RecurringTask<Void> {
     private final Logger LOG = LoggerFactory.getLogger(getClass());
