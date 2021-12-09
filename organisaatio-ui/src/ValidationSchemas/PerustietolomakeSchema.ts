932ec70d5ce887bd5dfe98a4a57b0d87ce9a36e2
Mikael Bärlund <mikael.barlund@wunderdog.fi>
2021-12-09 20:16:57 +0200

    require that organisaatioTyypit hase one item that is not organisaatiotyyppi_09 (Kunta)

--- organisaatio-ui/src/ValidationSchemas/PerustietolomakeSchema.ts
+++ organisaatio-ui/src/ValidationSchemas/PerustietolomakeSchema.ts
@@ -4,7 +4,7 @@ export default Joi.object({
     nimi: Joi.object({ fi: Joi.string(), sv: Joi.string(), en: Joi.string() }).required(),
     ytunnus: Joi.string().allow(''),
     alkuPvm: Joi.date().required(),
-    organisaatioTyypit: Joi.array().min(1).required(),
+    organisaatioTyypit: Joi.array().items(Joi.string()).has(Joi.string().not('organisaatiotyyppi_09').required()),
     kotipaikka: Joi.object({ label: Joi.string().required(), value: Joi.string().required() }).required(),
     muutKotipaikat: Joi.array(),
     maa: Joi.object({ label: Joi.string().required(), value: Joi.string().required() }).required(),
