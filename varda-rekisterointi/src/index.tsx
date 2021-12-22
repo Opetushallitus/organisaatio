9ad06431ab0e5c83bc64311afded015b686fb417
Lauri Arimo <lauri.arimo@wunderdog.fi>
2021-12-22 19:36:06 +0200

    OH-795: Version upgrade: Axios
    * 0.19.0 -> 0.24.0

--- varda-rekisterointi/src/index.tsx
+++ varda-rekisterointi/src/index.tsx
@@ -8,8 +8,11 @@ import Cookies from 'universal-cookie';
 const cookies = new Cookies();
 
 axios.interceptors.request.use(config => {
-    config.headers['Caller-Id'] = '1.2.246.562.10.00000000001.varda-rekisterointi';
-    config.headers['CSRF'] = cookies.get('CSRF');
+    const ophHeaders = {
+        'Caller-Id': '1.2.246.562.10.00000000001.varda-rekisterointi',
+        'CSRF': cookies.get('CSRF')
+    };
+    config.headers = { ...config.headers, ...ophHeaders };
     return config;
 });
 
