9f77e0c7d6b0ebd4c9fb6de5697fcbb3d04ce050
Mikael Bärlund <mikael.barlund@wunderdog.fi>
2021-12-23 15:59:59 +0200

    only show buttons and let modify if within own organisaatio

--- organisaatio-ui/cypress/integration/button_roles_spec.js
+++ organisaatio-ui/cypress/integration/button_roles_spec.js
@@ -1,7 +1,17 @@
 import { API_CONTEXT, BASE_PATH, PUBLIC_API_CONTEXT } from '../../src/contexts/constants';
 import { organisaatio } from '../support/data';
+import exp from 'constants';
 
 describe('Restrict buttons by roles', () => {
+    const baseCasMe = {
+        uid: 'mbender',
+        oid: '1.2.246.562.24.41721051355',
+        firstName: 'Mike',
+        lastName: 'Bender',
+        groups: ['LANG_fi', 'VIRKAILIJA'],
+        roles: '["APP_ORGANISAATIOHALLINTA","APP_ORGANISAATIOHALLINTA_CRUD","LANG_fi","USER_mbender","VIRKAILIJA"]',
+        lang: 'fi',
+    };
     const taulukkoLisaaUusi = 'TAULUKKO_LISAA_UUSI';
     const lomakeLisaaUusi = 'LISAA_UUSI';
     const oppilaitosButtons = ['SIIRRA', 'YHDISTA'];
@@ -19,7 +29,10 @@ describe('Restrict buttons by roles', () => {
         ...generalRestrictedButtons,
     ];
     it('Does not show buttons without required roles', () => {
-        cy.intercept('GET', `/kayttooikeus-service/cas/me`, { fixture: 'noRoles.json' });
+        cy.intercept('GET', `/kayttooikeus-service/cas/me`, {
+            ...baseCasMe,
+            roles: '["APP_ORGANISAATIOHALLINTA","APP_ORGANISAATIOHALLINTA_CRUD"]',
+        });
         cy.visit(`${BASE_PATH}/organisaatiot`);
         cy.get('h2', { timeout: 20000 }).contains('TAULUKKO_ORGANISAATIOT').should('exist');
         cy.get('button').contains(taulukkoLisaaUusi).should('not.exist');
@@ -67,7 +80,7 @@ describe('Restrict buttons by roles', () => {
         });
     });
     it('Shows buttons with CRUD roles', () => {
-        cy.intercept('GET', `/kayttooikeus-service/cas/me`, { fixture: 'CRUDRoles.json' });
+        cy.intercept('GET', `/kayttooikeus-service/cas/me`, baseCasMe);
         cy.visit(`${BASE_PATH}/organisaatiot`);
         cy.get('h2', { timeout: 20000 }).contains('TAULUKKO_ORGANISAATIOT').should('exist');
         cy.contains(taulukkoLisaaUusi).should('not.exist');
@@ -90,9 +103,14 @@ describe('Restrict buttons by roles', () => {
                 'grandchild'
             );
         });
-        cy.get('@parent').then((org) => {
+
+        cy.get('@parent').then((parent) => {
+            cy.intercept('GET', `/kayttooikeus-service/cas/me`, {
+                ...baseCasMe,
+                roles: `["APP_ORGANISAATIOHALLINTA_CRUD","APP_ORGANISAATIOHALLINTA_CRUD_${parent.body.organisaatio.oid}"]`,
+            });
             cy.log('test for Koulutustoimija');
-            cy.visit(`${BASE_PATH}/lomake/${org.body.organisaatio.oid}`);
+            cy.visit(`${BASE_PATH}/lomake/${parent.body.organisaatio.oid}`);
             cy.get('h3', { timeout: 20000 }).contains('Koulutustoimija').should('exist');
             allRestrictedButtons.forEach((a) => cy.get('button').contains(a).should('not.exist'));
             [suljeButton].forEach((a) => cy.get('button').contains(a).should('exist'));
--- organisaatio-ui/src/contexts/CasMeContext.ts
+++ organisaatio-ui/src/contexts/CasMeContext.ts
@@ -1,10 +1,24 @@
 import { CASMe, ConfigurableButton, Language, OrganisaatioNimiJaOid } from '../types/types';
 import * as React from 'react';
+import { ROOT_OID } from './constants';
 
-type Role = 'APP_ORGANISAATIOHALLINTA_CRUD_1.2.246.562.10.00000000001' | 'APP_ORGANISAATIOHALLINTA_CRUD';
-function getRoleItems<A>(myRole: Role, role: Role, items: A[]): A[] {
+const ORGANISAATIO_CRUD = 'APP_ORGANISAATIOHALLINTA_CRUD';
+const OPH_CRUD = `${ORGANISAATIO_CRUD}_${ROOT_OID}`;
+const getRoleItems = <A>(myRole: string, role: string, items: A[]): A[] => {
     return myRole === role ? items : [];
-}
+};
+export const organisationAllowedInRoles = (
+    organisaatioNimiPolku: OrganisaatioNimiJaOid[],
+    roles: string[]
+): boolean => {
+    const oidsFromRoles = roles
+        .filter((a) => a.startsWith(`${ORGANISAATIO_CRUD}_`))
+        .map((a) => a.substr(ORGANISAATIO_CRUD.length + 1));
+    const isAllowed = oidsFromRoles.reduce((p, c) => {
+        return p || !!organisaatioNimiPolku.find((a) => a.oid === c);
+    }, false);
+    return isAllowed;
+};
 
 export class CASMeImpl implements CASMe {
     firstName: string;
@@ -12,7 +26,7 @@ export class CASMeImpl implements CASMe {
     lang: Language;
     lastName: string;
     oid: string;
-    roles: Role[];
+    roles: string[];
     uid: string;
     allowedButtons: string[];
     constructor(casMe) {
@@ -24,21 +38,24 @@ export class CASMeImpl implements CASMe {
         this.roles = casMe.roles;
         this.uid = casMe.uid;
         this.allowedButtons = casMe.roles.reduce(
-            (p, c) => [
-                ...p,
-                ...getRoleItems<ConfigurableButton>(c, 'APP_ORGANISAATIOHALLINTA_CRUD', ['LOMAKE_LISAA_UUSI_TOIMIJA']),
-            ],
+            (p, c) => [...p, ...getRoleItems<ConfigurableButton>(c, ORGANISAATIO_CRUD, ['LOMAKE_LISAA_UUSI_TOIMIJA'])],
             [] as ConfigurableButton[]
         );
     }
     canHaveButton = (button: ConfigurableButton, organisaatioNimiPolku: OrganisaatioNimiJaOid[]) => {
-        if (this.roles.includes('APP_ORGANISAATIOHALLINTA_CRUD_1.2.246.562.10.00000000001')) return true;
-        if (this.roles.includes('APP_ORGANISAATIOHALLINTA_CRUD') && organisaatioNimiPolku.length > 2) return true;
-        return this.allowedButtons.includes(button) && organisaatioNimiPolku.length > 1;
+        if (this.roles.includes(OPH_CRUD)) return true;
+        if (organisaatioNimiPolku.length > 2 && organisationAllowedInRoles(organisaatioNimiPolku, this.roles))
+            return true;
+        return (
+            organisaatioNimiPolku.length > 1 &&
+            organisationAllowedInRoles(organisaatioNimiPolku, this.roles) &&
+            this.allowedButtons.includes(button)
+        );
     };
     canEditIfParent = (organisaatioNimiPolku: OrganisaatioNimiJaOid[]) => {
-        if (this.roles.includes('APP_ORGANISAATIOHALLINTA_CRUD_1.2.246.562.10.00000000001')) return true;
-        if (this.roles.includes('APP_ORGANISAATIOHALLINTA_CRUD') && organisaatioNimiPolku.length > 2) return true;
+        if (this.roles.includes(OPH_CRUD)) return true;
+        if (organisaatioNimiPolku.length > 2 && organisationAllowedInRoles(organisaatioNimiPolku, this.roles))
+            return true;
         return false;
     };
 }
