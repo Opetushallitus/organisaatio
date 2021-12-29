410ec138160d9b5704e4e51eba4387d40e623fef
Mikael Bärlund <mikael.barlund@wunderdog.fi>
2021-12-29 12:40:11 +0200

    set cookie for all clients & up java-http version

--- organisaatio-ui/scripts/jsonToCsv.js
+++ organisaatio-ui/scripts/jsonToCsv.js
@@ -1,6 +1,9 @@
-var fs = require('fs');
+const fs = require('fs');
+const { format } = require('@fast-csv/format');
 const input = 'localisation.json';
 const output = 'localisation.csv';
+const encodingOut = 'utf16le';
+const separator = '\t';
 fs.unlink(output, function (err) {
     if (err) console.log('error deleting');
     console.log('File deleted!');
@@ -8,9 +11,16 @@ fs.unlink(output, function (err) {
 fs.readFile(input, 'utf8', function (err, data) {
     const json = JSON.parse(data);
     const organisaatio2Data = json.filter((a) => a.category === 'organisaatio2');
-    fs.appendFile(output, '\uFEFFsep=,\n"category","key","value_fi","value_sv","value_en"\n', function (err) {
-        if (err) throw err;
-    });
+    fs.appendFile(
+        output,
+        '\ufeff' +
+            //'sep=, \n' +
+            `"category"${separator}"key"${separator}"value_fi"${separator}"value_sv"${separator}"value_en"`,
+        { encoding: encodingOut },
+        function (err) {
+            if (err) throw err;
+        }
+    );
     organisaatio2Data.sort((a, b) => {
         return a.key.localeCompare(b.key);
     });
@@ -20,8 +30,10 @@ fs.readFile(input, 'utf8', function (err, data) {
             const sv = organisaatio2Data.find((b) => b.key === a.key && b.locale === 'sv')?.value || '';
             const en = organisaatio2Data.find((b) => b.key === a.key && b.locale === 'en')?.value || '';
 
-            const line = `"${a.category}","${a.key}","${a.value || ''}","${sv}","${en}"\n`;
-            fs.appendFile(output, line, function (err) {
+            const line = `"${a.category}"${separator}"${a.key}${separator}"${
+                a.value || ''
+            }"${separator}"${sv}"${separator}"${en}"\n`;
+            fs.appendFile(output, line, { encoding: encodingOut }, function (err) {
                 if (err) throw err;
             });
         });
