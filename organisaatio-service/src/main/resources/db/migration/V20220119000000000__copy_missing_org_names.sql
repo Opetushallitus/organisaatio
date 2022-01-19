2b4de339264344891917fee5ae006de8c87e3f74
Mikael Bärlund <mikael.barlund@wunderdog.fi>
2022-01-19 16:12:11 +0200

    improve logging from migration

--- organisaatio-service/src/main/resources/db/migration/V20220119000000000__copy_missing_org_names.sql
+++ organisaatio-service/src/main/resources/db/migration/V20220119000000000__copy_missing_org_names.sql
@@ -1,15 +1,18 @@
-CREATE OR REPLACE FUNCTION migrate_organisaatio_names(varchar, varchar) RETURNS integer
-AS
+CREATE OR REPLACE FUNCTION migrate_organisaatio_names(_to varchar, _from varchar, OUT _rows varchar)
+    RETURNS varchar AS
 $$
 BEGIN
-    insert into monikielinenteksti_values (id, value, key, index)
-        (select mv2.id, mv2.value, $1, 0
-         from organisaatio o
-                  join monikielinenteksti m on o.nimi_mkt = m.id
-                  left join monikielinenteksti_values mv1 on m.id = mv1.id and mv1.key = $1
-                  join monikielinenteksti_values mv2 on m.id = mv2.id and mv2.key = $2
-         where mv1.id is null);
-    RETURN 1;
+    WITH rows AS (
+        insert into monikielinenteksti_values (id, value, key, index)
+            (select mv2.id, mv2.value, _to, 0
+             from organisaatio o
+                      join monikielinenteksti m on o.nimi_mkt = m.id
+                      left join monikielinenteksti_values mv1 on m.id = mv1.id and mv1.key = _to
+                      join monikielinenteksti_values mv2 on m.id = mv2.id and mv2.key = _from
+             where mv1.id is null)
+            RETURNING 1
+    )
+    SELECT CONCAT(count(*),' names inserted ') INTO _rows FROM rows;
 END;
 $$ LANGUAGE plpgsql;
 
@@ -20,8 +23,8 @@ where TRIM(mv.value) = ''
                                   from organisaatio o
                                            join monikielinenteksti m on m.id = o.nimi_mkt
                                   where m.id = mv.id);
-SELECT migrate_organisaatio_names('fi', 'sv'); -- sv --> fi
-SELECT migrate_organisaatio_names('fi', 'en'); -- en --> fi
-SELECT migrate_organisaatio_names('sv', 'fi'); -- fi --> sv
-SELECT migrate_organisaatio_names('en', 'fi'); -- fi --> en
-DROP FUNCTION migrate_organisaatio_names(varchar, varchar);
+SELECT migrate_organisaatio_names('fi', 'sv'),': sv --> fi';
+SELECT migrate_organisaatio_names('fi', 'en'),': en --> fi';
+SELECT migrate_organisaatio_names('sv', 'fi'),': fi --> sv';
+SELECT migrate_organisaatio_names('en', 'fi'),': fi --> en';
+DROP FUNCTION migrate_organisaatio_names;
