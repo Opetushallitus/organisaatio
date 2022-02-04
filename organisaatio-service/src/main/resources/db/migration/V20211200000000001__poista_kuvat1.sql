71980d6128f2ecc0712a811a3099d25c19a690d8
Mikael Bärlund <mikael.barlund@wunderdog.fi>
2022-02-04 12:20:51 +0200

    remove procedure

--- organisaatio-service/src/main/resources/db/migration/V20211200000000001__poista_kuvat1.sql
+++ organisaatio-service/src/main/resources/db/migration/V20211200000000001__poista_kuvat1.sql
@@ -1,17 +1,3 @@
-CREATE OR REPLACE PROCEDURE remove_binarydata()
-    LANGUAGE plpgsql
-AS
-$$
-BEGIN
-    update binarydata
-    set data = null
-    where id in (select b.id
-                 from binarydata b
-                 where lo_unlink(b.data) = 1
-                   and b.id in (select x.id from binarydata x where x.data is not null limit 50));
-END;
-$$;
-
 select 'initial lobs size', count(1)
 from (select distinct loid from pg_largeobject) as loids;
 do
@@ -21,7 +7,12 @@ $$
     begin
         while counter > 10000
             loop
-                call remove_binarydata();
+                update binarydata
+                set data = null
+                where id in (select b.id
+                             from binarydata b
+                             where lo_unlink(b.data) = 1
+                               and b.id in (select x.id from binarydata x where x.data is not null limit 50));
                 select count(1) into counter from binarydata where data is not null;
                 raise notice 'Rows left %', counter;
             end loop;
--- organisaatio-service/src/main/resources/db/migration/V20211200000000002__poista_kuvat2.sql
+++ organisaatio-service/src/main/resources/db/migration/V20211200000000002__poista_kuvat2.sql
@@ -1,17 +1,3 @@
-CREATE OR REPLACE PROCEDURE remove_binarydata()
-    LANGUAGE plpgsql
-AS
-$$
-BEGIN
-    update binarydata
-    set data = null
-    where id in (select b.id
-                 from binarydata b
-                 where lo_unlink(b.data) = 1
-                   and b.id in (select x.id from binarydata x where x.data is not null limit 50));
-END;
-$$;
-
 select 'initial lobs size', count(1)
 from (select distinct loid from pg_largeobject) as loids;
 do $$
@@ -19,7 +5,12 @@ do $$
         counter integer := 100000;
     begin
         while counter > 0 loop
-                call remove_binarydata();
+                update binarydata
+                set data = null
+                where id in (select b.id
+                             from binarydata b
+                             where lo_unlink(b.data) = 1
+                               and b.id in (select x.id from binarydata x where x.data is not null limit 50));
                 select count(1) into counter from binarydata where data is not null;
                 raise notice 'Rows left %', counter;
             end loop;
