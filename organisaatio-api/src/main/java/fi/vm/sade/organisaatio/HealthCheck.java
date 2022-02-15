756100c8cd37c942e2ce18ca1091aca18a12373d
Mikael Bärlund <mikael.barlund@wunderdog.fi>
2022-02-15 11:42:01 +0200

    OH-831 fix swagger-ui

--- organisaatio-api/src/main/java/fi/vm/sade/organisaatio/HealthCheck.java
+++ organisaatio-api/src/main/java/fi/vm/sade/organisaatio/HealthCheck.java
@@ -1,8 +1,9 @@
 package fi.vm.sade.organisaatio;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
-
+@Hidden
 public interface HealthCheck {
     @GetMapping(path = "/", produces = MediaType.TEXT_PLAIN_VALUE)
     String hello();
--- organisaatio-api/src/main/java/fi/vm/sade/organisaatio/resource/OrganisaatioApi.java
+++ organisaatio-api/src/main/java/fi/vm/sade/organisaatio/resource/OrganisaatioApi.java
@@ -4,8 +4,10 @@ import fi.vm.sade.organisaatio.api.DateParam;
 import fi.vm.sade.organisaatio.dto.OrganisaatioNimiDTO;
 import fi.vm.sade.organisaatio.dto.OrganisaatioNimiUpdateDTO;
 import fi.vm.sade.organisaatio.dto.v4.*;
+import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import org.springdoc.api.annotations.ParameterObject;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,53 +27,37 @@ import java.util.Set;
 
 public interface OrganisaatioApi {
 
+    @Operation(summary = "Hakee monta ei-poistettua organisaatiota kerralla syötetyille OIDeille (maksimissaan 1000)")
     @PostMapping(path = "/findbyoids", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    @Operation(
-            summary = "Hakee monta ei-poistettua organisaatiota kerralla syötetyille OIDeille (maksimissaan 1000)")
-    List<OrganisaatioRDTOV4> findByOids(
-            @Parameter(description = "JSON-taulukko organisaatio OIDeja: [\"oid1\", \"oid2\", ...]", required = true) @RequestBody Set<String> oids
-    );
+    List<OrganisaatioRDTOV4> findByOids(@RequestBody Set<String> oids);
 
+    @Operation(summary = "Hakee organisaation alla olevat organisaatiot")
     @GetMapping(path = "/{oid}/children", produces = MediaType.APPLICATION_JSON_VALUE)
-    @Operation(
-            summary = "Hakee organisaation alla olevat organisaatiot",
-            description = "Operaatio palauttaa organisaation alla olevat organisaatiot.")
     List<OrganisaatioRDTOV4> children(
             @Parameter(description = "Organisaation oid", required = true) @PathVariable String oid,
-            @Parameter(description = "Palaulautetaanko vastauksen mukana mahdollinen organisaation kuva (voi olla iso).") @RequestParam(defaultValue = "false") boolean includeImage
+            @Parameter(description = "Palaulautetaanko vastauksen mukana mahdollinen organisaation kuva (voi olla iso).", deprecated = true) @RequestParam(defaultValue = "false") boolean includeImage
     );
 
+    @Operation(summary = "Hakee yhden organisaation annetulla id:llä (id voi olla oid, y-tunnus, virastotunnus, oppilaitoskoodi tai toimipistekoodi).")
     @GetMapping(path = "/{oid}", produces = MediaType.APPLICATION_JSON_VALUE)
-    @Operation(
-            summary = "Hakee yhden organisaation annetulla id:llä (id voi olla oid, y-tunnus, virastotunnus, oppilaitoskoodi tai toimipistekoodi).",
-            description = "Operaatio palauttaa id:n määrittämän organisaation tiedot.")
     OrganisaatioRDTOV4 getOrganisaatioByOID(
             @Parameter(description = "Organisaation oid, y-tunnus, virastotunnus, oppilaitoskoodi tai toimipistekoodi.", required = true) @PathVariable String oid,
-            @Parameter(description = "Palaulautetaanko vastauksen mukana mahdollinen organisaation kuva (voi olla iso).") @RequestParam(defaultValue = "false") boolean includeImage
+            @Parameter(description = "Palaulautetaanko vastauksen mukana mahdollinen organisaation kuva (voi olla iso).", deprecated = true) @RequestParam(defaultValue = "false") boolean includeImage
     );
 
+    @Operation(summary = "Päivittää oid:n määrittämän organisaation tiedot")
     @PutMapping(path = "/{oid}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    @Parameter(name = "organisaatio", description = "Organisaation tiedot json muodossa")
-    @Operation(
-            summary = "Päivittää oid:n määrittämän organisaation tiedot",
-            description = "Operaatio päivittää oid:n määrittämän organisaation tiedot.")
     ResultRDTOV4 updateOrganisaatio(
             @Parameter(description = "Organisaation oid", required = true) @PathVariable String oid,
-            @Parameter(description = "hidden") @RequestBody OrganisaatioRDTOV4 ordto
+            @RequestBody OrganisaatioRDTOV4 ordto
     );
 
+    @Operation(summary = "Operaatio luo uuden organisaation annetusta JSON:sta.")
     @PostMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    @Parameter(name = "organisaatio", description = "Luotavan organisaation tiedot json muodossa")
-    @Operation(
-            summary = "Luo uuden organisaation",
-            description = "Operaatio luo uuden organisaation annetusta JSON:sta.")
-    ResultRDTOV4 newOrganisaatio(
-            @Parameter(hidden = true) @RequestBody OrganisaatioRDTOV4 ordto
-    );
+    ResultRDTOV4 newOrganisaatio(@RequestBody OrganisaatioRDTOV4 ordto);
 
+    @Operation(summary = "Hakee organisaatioiden tiedot, joita muutettu annetun päivämäärän jälkeen")
     @GetMapping(path = "/muutetut", produces = MediaType.APPLICATION_JSON_VALUE)
-    @Operation(
-            summary = "Hakee organisaatioiden tiedot, joita muutettu annetun päivämäärän jälkeen")
     List<OrganisaatioRDTOV4> haeMuutetut(
             @Parameter(description = "Muokattu jälkeen", required = true) @RequestParam DateParam lastModifiedSince,
             @Parameter(description = "Palaulautetaanko vastauksen mukana mahdollinen organisaation kuva (voi olla iso).") @RequestParam(defaultValue = "false") boolean includeImage,
@@ -79,46 +65,16 @@ public interface OrganisaatioApi {
             @Parameter(description = "Rajataanko lakkautetut organisaatiot pois") @RequestParam(defaultValue = "false") boolean excludeDiscontinued
     );
 
+    @Operation(summary = "Hakee organisaation rakennehistorian.")
     @GetMapping(path = "/{oid}/historia", produces = MediaType.APPLICATION_JSON_VALUE)
-    @Operation(
-            summary = "Hakee organisaation rakennehistorian.",
-            description = "Operaatio palauttaa oid:n määrittelemän organisaation rakennehistorian.")
     OrganisaatioHistoriaRDTOV4 getOrganizationHistory(
             @Parameter(description = "Organisaation oid", required = true) @PathVariable String oid
     );
 
     @GetMapping(path = "/hae", produces = MediaType.APPLICATION_JSON_VALUE)
-    @Parameter(name = "searchStr", description = "Hakuteksti")
-    @Parameter(name = "aktiiviset", description = "Aktiiviset organisaatiot mukaan hakutuloksiin", required = true)
-    @Parameter(name = "suunnitellut", description = "Suunnitellut organisaatiot mukaan hakutuloksiin", required = true)
-    @Parameter(name = "lakkautetut", description = "Lakkautetut organisaatiot mukaan hakutuloksiin", required = true)
-    @Parameter(name = "yritysmuoto", description = "Haettavan organisaation yritysmuoto tai lista yritysmuodoista")
-    @Parameter(name = "kunta", description = "Haettavan organisaation kunta tai lista kunnista")
-    @Parameter(name = "organisaatiotyyppi", description = "Haettavan organisaation tyyppi koodiarvona")
-    @Parameter(name = "oppilaitostyyppi", description = "Haettavan oppilaitoksen tyyppi tai lista tyypeistä")
-    @Parameter(name = "kieli", description = "Haettavan organisaation kieli tai lista kielistä")
-    @Parameter(name = "oidRestrictionList", description = "Lista sallituista organisaatioiden oid:stä")
-    @Parameter(name = "oid", description = "Haku oid:lla. Hakuteksti jätetään huomioimatta jos oid on annettu.")
-    @Parameter(name = "skipParents", description = "Jätetäänkö yläorganisaatiot pois hakutuloksista")
-    @Operation(
-            summary = "Hakee organisaatiot, jotka osuvat annetuihin hakuehtoihin",
-            description = "Operaatio palauttaa vain hakuehtoja vastaavat organisaatiot.")
-    OrganisaatioHakutulosV4 searchOrganisaatiot(
-            @Parameter(hidden = true) OrganisaatioSearchCriteriaDTOV4 hakuEhdot
-    );
+    OrganisaatioHakutulosV4 searchOrganisaatiot(@ParameterObject() OrganisaatioSearchCriteriaDTOV4 hakuEhdot);
 
-    @GetMapping(path = "/hierarkia/hae", produces = MediaType.APPLICATION_JSON_VALUE)
-    @Parameter(name = "searchStr", description = "Hakuteksti")
-    @Parameter(name = "aktiiviset", description = "Aktiiviset organisaatiot mukaan hakutuloksiin", required = true)
-    @Parameter(name = "suunnitellut", description = "Suunnitellut organisaatiot mukaan hakutuloksiin", required = true)
-    @Parameter(name = "lakkautetut", description = "Lakkautetut organisaatiot mukaan hakutuloksiin", required = true)
-    @Parameter(name = "kunta", description = "Haettavan organisaation kunta tai lista kunnista")
-    @Parameter(name = "organisaatiotyyppi", description = "Haettavan organisaation tyyppi koodiarvona")
-    @Parameter(name = "oppilaitostyyppi", description = "Haettavan oppilaitoksen tyyppi tai lista tyypeistä")
-    @Parameter(name = "kieli", description = "Haettavan organisaation kieli tai lista kielistä")
-    @Parameter(name = "oidRestrictionList", description = "Lista sallituista organisaatioiden oid:stä")
-    @Parameter(name = "oid", description = "Haku oid:lla. Hakuteksti jätetään huomioimatta jos oid on annettu.")
-    @Parameter(name = "skipParents", description = "Jätetäänkö yläorganisaatiot pois hakutuloksista")
+
     @Operation(
             summary = "Hakee organisaatiot puurakenteena annetuilla hakuehdoilla",
             description = "Operaatio palauttaa hakuehtoja vastaavat organisaatiot puurakenteena. "
@@ -126,18 +82,17 @@ public interface OrganisaatioApi {
                     + "palautetaan alemman tason organisaatio myös, siis puurakenne lehtiin asti."
                     + "Hakuehtojen osuessa hierarkiassa alemman tason organisaatioon, "
                     + "palautetaan puurakenne juureen asti (ellei hakuehdot sitä estä).")
-    OrganisaatioHakutulosV4 searchOrganisaatioHierarkia(
-            @Parameter(hidden = true) OrganisaatioSearchCriteriaDTOV4 hakuEhdot
-    );
+    @GetMapping(path = "/hierarkia/hae", produces = MediaType.APPLICATION_JSON_VALUE)
+    OrganisaatioHakutulosV4 searchOrganisaatioHierarkia(@ParameterObject() OrganisaatioSearchCriteriaDTOV4 hakuEhdot);
 
+    @Operation(summary = "Palauttaa organisaation jälkeläiset")
     @GetMapping(path = "/{oid}/jalkelaiset", produces = MediaType.APPLICATION_JSON_VALUE)
     OrganisaatioHakutulosV4 findDescendants(
             @Parameter(description = "Organisaation oid", required = true) @PathVariable String oid
     );
 
+    @Operation(summary = "Siirtää organisaatiota puussa toisen ylemmän organisaation alle tai yhdistää kaksi samanarvoista organisaatiota")
     @PutMapping(path = "/{oid}/organisaatiosuhde/{parentoid}", produces = MediaType.APPLICATION_JSON_VALUE)
-    @Operation(
-            summary = "Siirtää organisaatiota puussa toisen ylemmän organisaation alle tai yhdistää kaksi samanarvoista organisaatiota")
     OrganisaatioRDTOV4 changeOrganisationRelationship(
             @Parameter(description = "Organisaation oid", required = true) @PathVariable("oid") String oid,
             @Parameter(description = "Uusi isäntäorganisaatio", required = true) @PathVariable("parentoid") String parentoid,
@@ -145,35 +100,32 @@ public interface OrganisaatioApi {
             @Parameter(description = "Siirto päivämäärä, jos päivämäärää ei ole asetettu käytetään tätä päivämäärää", required = true) @RequestParam("moveDate") DateParam moveDate
     );
 
-    @DeleteMapping(path = "/{oid}")
-    @Operation(
-            summary = "Poistaa oid:n määrittämän organisaation",
-            description = "Operaatio poistaa organisaation annetulla oid:llä.")
-    void deleteOrganisaatio(
-            @Parameter(description = "Organisaation oid", required = true) @PathVariable String oid
-    );
 
+    @Operation(summary = "Poistaa oid:n määrittämän organisaation")
+    @DeleteMapping(path = "/{oid}")
+    void deleteOrganisaatio(@Parameter(description = "Organisaation oid", required = true) @PathVariable("oid") String oid);
+
+    @Operation(summary = "Palauttaa organisaation viimeinen päivittäjä.")
     @GetMapping(path = "/{oid}/paivittaja", produces = MediaType.APPLICATION_JSON_VALUE)
-    OrganisaatioPaivittajaDTO getOrganisaatioPaivittaja(@PathVariable("oid") String oid);
+    OrganisaatioPaivittajaDTO getOrganisaatioPaivittaja(@Parameter(description = "Organisaation oid", required = true) @PathVariable("oid") String oid);
 
     // nimen muokkausta varten alla:
-
-    // Operaatio luo uuden nimen organisaatiolle annetusta JSON:sta.
+    @Operation(summary = "Operaatio luo uuden nimen organisaatiolle annetusta JSON:sta.")
     @PostMapping(path = "/{oid}/nimet", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    OrganisaatioNimiDTO newOrganisaatioNimi(@PathVariable("oid") String oid, @RequestBody OrganisaatioNimiDTO nimidto);
+    OrganisaatioNimiDTO newOrganisaatioNimi(@Parameter(description = "Organisaation oid", required = true) @PathVariable("oid") String oid, @RequestBody OrganisaatioNimiDTO nimidto);
 
-    // Operaatio päivittää oid:n määrittämän organisaation nimen, jonka aikaisempi alkupäivämäärä on annettu date.
+    @Operation(summary = "Operaatio päivittää oid:n määrittämän organisaation nimen, jonka aikaisempi alkupäivämäärä on annettu date.")
     @PutMapping(path = "/{oid}/nimet", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    OrganisaatioNimiDTO updateOrganisaatioNimi(@PathVariable("oid") String oid, @RequestBody OrganisaatioNimiUpdateDTO nimiUpdateDto);
+    OrganisaatioNimiDTO updateOrganisaatioNimi(@Parameter(description = "Organisaation oid", required = true) @PathVariable("oid") String oid, @RequestBody OrganisaatioNimiUpdateDTO nimiUpdateDto);
 
-    // Operaatio poistaa oid:n määrittämän organisaation nimen, jonka aikaisempi alkupäivämäärä on annettu date.
+    @Operation(summary = "Operaatio poistaa oid:n määrittämän organisaation nimen, jonka aikaisempi alkupäivämäärä on annettu date.")
     @DeleteMapping(path = "/{oid}/nimet")
-    void deleteOrganisaatioNimi(@PathVariable("oid") String oid, @RequestBody OrganisaatioNimiDTO nimidto);
+    void deleteOrganisaatioNimi(@Parameter(description = "Organisaation oid", required = true) @PathVariable("oid") String oid, @RequestBody OrganisaatioNimiDTO nimidto);
 
-    @Operation(
-            summary = "Päivittää organisaation tarkastusPvm nykyhetkeen.")
+    @Hidden
+    @Operation(summary = "Operaatio asettaa organisaatiolle nykyhetken tarkastuspäivämääräksi ja palauttaa tarkastuksen aikaleiman.")
     @PutMapping(path = "/{oid}/tarkasta", produces = MediaType.APPLICATION_JSON_VALUE)
-    Timestamp updateTarkastusPvm(@PathVariable("oid") String oid);
+    Timestamp updateTarkastusPvm(@Parameter(description = "Organisaation oid", required = true) @PathVariable("oid") String oid);
 
 
 }
