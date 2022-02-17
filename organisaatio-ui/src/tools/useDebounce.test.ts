5813fa00985e9f538f26bfd4fedb4233fc62d5e9
mkamka <miikka.kajanne@wunderdog.fi>
2022-02-17 11:18:51 +0200

    korjattu testi toimimaan oikein.

--- organisaatio-ui/src/tools/useDebounce.test.ts
+++ organisaatio-ui/src/tools/useDebounce.test.ts
@@ -2,7 +2,7 @@ import { renderHook } from '@testing-library/react-hooks';
 import useDebounce from './useDebounce';
 
 describe('useDebounce', () => {
-    it('should update debounced value after delay passes', () => {
+    it('should update debounced value after delay passes', (done) => {
         const { result, rerender } = renderHook(({ searchStr, delay = 1000 }) => useDebounce(searchStr, delay), {
             initialProps: { searchStr: '', delay: 1000 } as { searchStr: string; delay?: number },
         });
@@ -12,20 +12,20 @@ describe('useDebounce', () => {
         rerender({ searchStr: 'moikka' });
         expect(result.current).toEqual('');
         setTimeout(() => {
-            rerender({ searchStr: 'moikkamoi' });
-            expect(result.current).toEqual('moikkamoi');
+            expect(result.current).toEqual('moikka');
+            done();
         }, 1001);
     });
-    it('should update debounced value based on updated delay also', () => {
+    it('should update debounced value based on updated delay also', (done) => {
         const { result, rerender } = renderHook(({ searchStr, delay = 1000 }) => useDebounce(searchStr, delay), {
             initialProps: { searchStr: '', delay: 1000 } as { searchStr: string; delay?: number },
         });
-        expect(result.current).toEqual('');
         rerender({ searchStr: 'moikka', delay: 350 });
-        rerender({ searchStr: 'moikkamoi1' });
+        rerender({ searchStr: 'moikkamoi1', delay: 350 });
+        expect(result.current).toEqual('');
         setTimeout(() => {
-            rerender({ searchStr: 'moikkamoi' });
-            expect(result.current).toEqual('moikkamoi');
-        }, 351);
+            expect(result.current).toEqual('moikkamoi1');
+            done();
+        }, 352);
     });
 });
