f1c1c090fb99440a9fd3993e7cea6875f0f1abc8
mkamka <miikka.kajanne@wunderdog.fi>
2022-08-10 12:42:11 +0300

    Spring Boot base

---
+++ rekisterointi/src/main/java/fi/vm/sade/rekisterointi/RekisterointiApplication.java
@@ -0,0 +1,13 @@
+package fi.vm.sade.rekisterointi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RekisterointiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RekisterointiApplication.class, args);
+	}
+
+}
---
+++ rekisterointi/src/test/java/fi/vm/sade/rekisterointi/RekisterointiApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.vm.sade.rekisterointi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RekisterointiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
