521b99d7aa961075a10c6a84d2a1b3f111680593
mkamka <miikka.kajanne@wunderdog.fi>
2022-09-02 16:28:50 +0300

    Vielä propertyihin muutoksia.

--- rekisterointi/src/main/java/fi/vm/sade/rekisterointi/configuration/PropertiesConfiguration.java
+++ rekisterointi/src/main/java/fi/vm/sade/rekisterointi/configuration/PropertiesConfiguration.java
@@ -26,6 +26,7 @@ public class PropertiesConfiguration {
       properties = new OphProperties("/rekisterointi_oph.properties");
       properties.addDefault("url-oppija", environment.getRequiredProperty("url-oppija"));
       properties.addDefault("url-virkailija", environment.getRequiredProperty("url-virkailija"));
+      properties.addDefault("url-rekisterointi", environment.getRequiredProperty("url-virkailija"));
       properties.addDefault("url-alb", environment.getRequiredProperty("url-alb"));
     }
     return properties;
