946621a5d0d1a3f89d2e6a4be211d48df498b92b
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2022-09-07 17:23:43 +0300

    add csrf and caller-id headers

--- rekisterointi/rekisterointi-ui/src/index.tsx
+++ rekisterointi/rekisterointi-ui/src/index.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import axios from 'axios';
+import Cookies from 'universal-cookie';
 
 import App from './App';
+
+const cookies = new Cookies();
+
+axios.interceptors.request.use((config) => {
+    const ophHeaders = {
+        'Caller-Id': '1.2.246.562.10.00000000001.varda-rekisterointi',
+        CSRF: cookies.get('CSRF'),
+    };
+    config.headers = { ...config.headers, ...ophHeaders };
+    return config;
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
