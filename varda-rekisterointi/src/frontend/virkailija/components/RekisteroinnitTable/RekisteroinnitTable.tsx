b086e0c29e7e75a54906df66ed1a6ae29f06aaf3
mkamka <miikka.kajanne@wunderdog.fi>
2022-09-26 15:07:53 +0300

    turha filterFn attribuutti vielä pois.

--- varda-rekisterointi/src/frontend/virkailija/components/RekisteroinnitTable/RekisteroinnitTable.tsx
+++ varda-rekisterointi/src/frontend/virkailija/components/RekisteroinnitTable/RekisteroinnitTable.tsx
@@ -100,7 +100,6 @@ export default function RekisteroinnitTable({ rekisteroinnit }: RekisteroinnitTa
             {
                 enableColumnFilter: true,
                 accessorKey: 'tila',
-                filterFn: 'arrIncludes',
             },
         ],
         [i18n]
