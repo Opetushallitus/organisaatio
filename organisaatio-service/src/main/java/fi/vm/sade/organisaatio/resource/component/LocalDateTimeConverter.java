50f13dd51f2aa85b661ac45c16530296528d64e3
Mikael Bärlund <mikael.barlund@wunderdog.fi>
2022-10-14 10:23:34 +0300

    refactor datetimeconverter

--- organisaatio-service/src/main/java/fi/vm/sade/organisaatio/resource/component/LocalDateTimeConverter.java
+++ organisaatio-service/src/main/java/fi/vm/sade/organisaatio/resource/component/LocalDateTimeConverter.java
@@ -8,7 +8,6 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
