b71923269d431a5459f3e309e1bad2ce9412935c
Timo Suomela <timo.suomela@reaktor.com>
2023-09-14 11:39:59 +0300

    asdf

--- playwright/playwright.config.ts
+++ playwright/playwright.config.ts
@@ -17,6 +17,11 @@ export default defineConfig({
     },
 
     {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+
+    {
       name: "webkit",
       use: { ...devices["Desktop Safari"] },
     },
