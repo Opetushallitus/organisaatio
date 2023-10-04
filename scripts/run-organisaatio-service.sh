f6e2af264d55e2dd94ff6205b911ab3187de3e0b
Timo Suomela <timo.suomela@reaktor.com>
2023-10-04 12:04:48 +0300

    OSTJ-193 Don't compile, lint, etc the ui files when starting the server

--- scripts/run-organisaatio-service.sh
+++ scripts/run-organisaatio-service.sh
@@ -3,7 +3,7 @@ set -o errexit -o nounset -o pipefail
 readonly repo="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 
 function main {
-  "$repo"/gradlew :organisaatio-service:run
+  "$repo"/gradlew :organisaatio-service:run --exclude-task :organisaatio-ui:jar
 }
 
 main "$@"
