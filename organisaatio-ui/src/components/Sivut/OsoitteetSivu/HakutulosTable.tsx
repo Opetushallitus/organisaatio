2fb191427bdb0214dd958ef6a26c097aa7c8f7ee
Henry Heikkinen <henry.heikkinen@reaktor.com>
2023-10-05 14:40:08 +0300

    OSTJ-193 Limit column widths and fade out too long texts

--- organisaatio-ui/src/components/Sivut/OsoitteetSivu/HakutulosTable.tsx
+++ organisaatio-ui/src/components/Sivut/OsoitteetSivu/HakutulosTable.tsx
@@ -12,45 +12,45 @@ export function HakutulosTable({ results }: HakutulosTableProps) {
             <table className={styles.Table}>
                 <thead className={styles.Header}>
                     <tr>
-                        <td className={styles.CheckboxColumn}>
+                        <td className={styles.ColumnCheckbox}>
                             <span className={styles.Checkbox}>
                                 <Checkbox checked={true} disabled={true} />
                             </span>
                             Organisaation nimi
                         </td>
-                        <td>Sähköpostiosoite</td>
-                        <td>Organisaatio OID</td>
-                        <td>Yritysmuoto</td>
+                        <td className={styles.ColumnEmail}>Sähköpostiosoite</td>
+                        <td className={styles.ColumnOid}>Organisaatio OID</td>
+                        <td className={styles.ColumnYritysmuoto}>Yritysmuoto</td>
                         <td>Puhelinnumero</td>
                         <td>Opetuskieli</td>
                         <td>Oppilaitostunnus</td>
                         <td>Kunta</td>
                         <td>Koski-virheilmoituksen osoite</td>
                         <td>Y-tunnus</td>
-                        <td>Postiosoite</td>
-                        <td>Käyntiosoite</td>
+                        <td className={styles.ColumnOsoite}>Postiosoite</td>
+                        <td className={styles.ColumnOsoite}>Käyntiosoite</td>
                     </tr>
                 </thead>
                 <tbody className={styles.Body}>
                     {results.map((_) => (
                         <tr key={_.id}>
-                            <td className={styles.CheckboxColumn}>
+                            <td className={styles.ColumnCheckbox + ' ' + styles.Fade}>
                                 <span className={styles.Checkbox}>
                                     <Checkbox checked={true} disabled={true} />
                                 </span>
                                 {_.nimi}
                             </td>
-                            <td>{_.sahkoposti ?? '-'}</td>
-                            <td>{_.oid}</td>
-                            <td>{_.yritysmuoto}</td>
+                            <td className={styles.ColumnEmail + ' ' + styles.Fade}>{_.sahkoposti ?? '-'}</td>
+                            <td className={styles.ColumnOid + ' ' + styles.Fade}>{_.oid}</td>
+                            <td className={styles.ColumnYritysmuoto + ' ' + styles.Fade}>{_.yritysmuoto}</td>
                             <td>{_.puhelinnumero ?? '-'}</td>
                             <td>{_.opetuskieli ?? '-'}</td>
                             <td>{_.oppilaitostunnus ?? '-'}</td>
                             <td>{_.kunta}</td>
                             <td>{_.koskiVirheilmoituksenOsoite ?? '-'}</td>
                             <td>{_.ytunnus}</td>
-                            <td>{_.postiosoite ?? '-'}</td>
-                            <td>{_.kayntiosoite ?? '-'}</td>
+                            <td className={styles.ColumnOsoite + ' ' + styles.Fade}>{_.postiosoite ?? '-'}</td>
+                            <td className={styles.ColumnOsoite + ' ' + styles.Fade}>{_.kayntiosoite ?? '-'}</td>
                         </tr>
                     ))}
                 </tbody>
