e1cbd8e77d4d478582defc635859d7e0c993a477
Henry Heikkinen <henry.heikkinen@reaktor.com>
2023-10-05 15:31:13 +0300

    OSTJ-193 Fix table column order to match designs

--- organisaatio-ui/src/components/Sivut/OsoitteetSivu/HakutulosTable.tsx
+++ organisaatio-ui/src/components/Sivut/OsoitteetSivu/HakutulosTable.tsx
@@ -19,13 +19,13 @@ export function HakutulosTable({ results }: HakutulosTableProps) {
                             Organisaation nimi
                         </td>
                         <td className={styles.ColumnEmail}>Sähköpostiosoite</td>
-                        <td className={styles.ColumnOid}>Organisaatio OID</td>
-                        <td className={styles.ColumnYritysmuoto}>Yritysmuoto</td>
                         <td>Puhelinnumero</td>
+                        <td>Sijaintikunta</td>
+                        <td className={styles.ColumnYritysmuoto}>Yritysmuoto</td>
                         <td>Opetuskieli</td>
+                        <td>KOSKI-virheilmoituksen osoite</td>
+                        <td className={styles.ColumnOid}>Organisaation OID</td>
                         <td>Oppilaitostunnus</td>
-                        <td>Kunta</td>
-                        <td>Koski-virheilmoituksen osoite</td>
                         <td>Y-tunnus</td>
                         <td className={styles.ColumnOsoite}>Postiosoite</td>
                         <td className={styles.ColumnOsoite}>Käyntiosoite</td>
@@ -41,13 +41,13 @@ export function HakutulosTable({ results }: HakutulosTableProps) {
                                 {_.nimi}
                             </td>
                             <td className={styles.ColumnEmail + ' ' + styles.Fade}>{_.sahkoposti ?? '-'}</td>
-                            <td className={styles.ColumnOid + ' ' + styles.Fade}>{_.oid}</td>
-                            <td className={styles.ColumnYritysmuoto + ' ' + styles.Fade}>{_.yritysmuoto}</td>
                             <td>{_.puhelinnumero ?? '-'}</td>
-                            <td>{_.opetuskieli ?? '-'}</td>
-                            <td>{_.oppilaitostunnus ?? '-'}</td>
                             <td>{_.kunta}</td>
+                            <td className={styles.ColumnYritysmuoto + ' ' + styles.Fade}>{_.yritysmuoto}</td>
+                            <td>{_.opetuskieli ?? '-'}</td>
                             <td>{_.koskiVirheilmoituksenOsoite ?? '-'}</td>
+                            <td className={styles.ColumnOid + ' ' + styles.Fade}>{_.oid}</td>
+                            <td>{_.oppilaitostunnus ?? '-'}</td>
                             <td>{_.ytunnus}</td>
                             <td className={styles.ColumnOsoite + ' ' + styles.Fade}>{_.postiosoite ?? '-'}</td>
                             <td className={styles.ColumnOsoite + ' ' + styles.Fade}>{_.kayntiosoite ?? '-'}</td>
