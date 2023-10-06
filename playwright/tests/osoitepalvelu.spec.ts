2c309b7c1bcbb243d960df317b14f702da55917c
Timo Suomela <timo.suomela@reaktor.com>
2023-10-06 19:35:12 +0300

    OSTJ-193 Extract common setup code

--- playwright/tests/osoitepalvelu.spec.ts
+++ playwright/tests/osoitepalvelu.spec.ts
@@ -1,20 +1,22 @@
-import {expect, test} from "@playwright/test";
+import { expect, test } from "@playwright/test";
 
 test.describe("Osoitepalvelu", () => {
-    test("has title", async ({page}) => {
-        await page.goto("http://localhost:3003/osoitteet");
+  test.beforeEach(async ({ page }, testInfo) => {
+    await page.goto("http://localhost:3003/osoitteet");
+  });
 
-        await expect(page).toHaveTitle(/Organisaatio/);
-    });
-    test("allows searching for koulutustoimijat", async ({page}) => {
-        await page.goto("http://localhost:3003/osoitteet");
+  test("has title", async ({ page }) => {
+    await expect(page).toHaveTitle(/Organisaatio/);
+  });
+  test("allows searching for koulutustoimijat", async ({ page }) => {
+    await expect(page.getByText("Hae")).toBeVisible();
+    await page.getByText("Hae").click();
 
-        await expect(page.getByText("Hae")).toBeVisible();
-        await page.getByText("Hae").click();
-
-        await expect(page.getByText("3 hakutulosta valittu")).toBeVisible();
-        await expect(page.getByText("Mansikkalan testi kunta")).toBeVisible();
-        await expect(page.getByText("Testi Koulutuskuntayhtymä Puolukka")).toBeVisible();
-        await expect(page.getByText("Mustikkalan testi yhdistys")).toBeVisible();
-    });
+    await expect(page.getByText("3 hakutulosta valittu")).toBeVisible();
+    await expect(page.getByText("Mansikkalan testi kunta")).toBeVisible();
+    await expect(
+      page.getByText("Testi Koulutuskuntayhtymä Puolukka")
+    ).toBeVisible();
+    await expect(page.getByText("Mustikkalan testi yhdistys")).toBeVisible();
+  });
 });
