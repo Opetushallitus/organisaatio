821311ca039cbceefccb3a17f63b223e8b77da67
Henry Heikkinen <henry.heikkinen@reaktor.com>
2023-10-12 09:58:32 +0300

    Handle errors in YTJ mock to prevent mock app from crashing

--- mock-api/src/api/ytj/POST.js
+++ mock-api/src/api/ytj/POST.js
@@ -1,11 +1,16 @@
 module.exports = function (request, response) {
-    const body = request.body['soap:envelope']['soap:body'][0];
-    if (body.wmyritystiedotv2) {
-        const yTunnus = body.wmyritystiedotv2[0].ytunnus[0];
-        response.sendFile(`${yTunnus}.xml`, { root: __dirname });
-    } else if (body.wmyrityshaku) {
-        const hakusana = body.wmyrityshaku[0].hakusana[0];
-        const shortHakusana = hakusana.toLowerCase().substring(0, 6);
-        response.sendFile(`${shortHakusana}.xml`, { root: __dirname });
+    try {
+        const body = request.body['soap:envelope']['soap:body'][0];
+        if (body.wmyritystiedotv2) {
+            const yTunnus = body.wmyritystiedotv2[0].ytunnus[0];
+            response.sendFile(`${yTunnus}.xml`, { root: __dirname });
+        } else if (body.wmyrityshaku) {
+            const hakusana = body.wmyrityshaku[0].hakusana[0];
+            const shortHakusana = hakusana.toLowerCase().substring(0, 6);
+            response.sendFile(`${shortHakusana}.xml`, { root: __dirname });
+        }
+    } catch (e) {
+        console.error(e);
+        response.status(500).send(e);
     }
 };
