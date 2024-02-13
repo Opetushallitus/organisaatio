ab9f6170e298d0c5c2b67407cf1ae8236a3ae375
Henry Heikkinen <henry.heikkinen@reaktor.com>
2024-02-13 16:07:20 +0200

    Replace LinklikeButton div with existing button component with same linklike style available

--- organisaatio-ui/src/components/Sivut/OsoitteetSivu/LinklikeButton.tsx
+++ organisaatio-ui/src/components/Sivut/OsoitteetSivu/LinklikeButton.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import styles from './LinklikeButton.module.css';
+import Button from '@opetushallitus/virkailija-ui-components/Button';
 
 export type LinklikeButtonProps = React.PropsWithChildren<{
     disabled?: boolean;
-    onClick?: () => void;
+    onClick: () => void;
 }>;
 
-function noop() {}
-
-export function LinklikeButton({ onClick = noop, disabled = false, children }: LinklikeButtonProps) {
+export function LinklikeButton({ onClick, disabled = false, children }: LinklikeButtonProps) {
     const classes = [styles.LinklikeButton];
     if (disabled) classes.push(styles.LinklikeButtonDisabled);
     return (
-        <div className={classes.join(' ')} role="button" onClick={onClick} aria-disabled={disabled}>
+        <Button variant={'text'} disabled={disabled} className={classes.join(' ')} onClick={onClick}>
             {children}
-        </div>
+        </Button>
     );
 }
