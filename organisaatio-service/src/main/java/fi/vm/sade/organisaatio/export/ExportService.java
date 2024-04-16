d1ee87cecb48e5b13966fad63bc90b1883fb7bb2
Timo Suomela <timo.suomela@reaktor.com>
2024-04-16 16:16:56 +0300

    OPHYK-65 Upload csv export files to Lampi

--- organisaatio-service/src/main/java/fi/vm/sade/organisaatio/export/ExportService.java
+++ organisaatio-service/src/main/java/fi/vm/sade/organisaatio/export/ExportService.java
@@ -1,21 +1,43 @@
 package fi.vm.sade.organisaatio.export;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import software.amazon.awssdk.core.async.AsyncRequestBody;
+import software.amazon.awssdk.services.s3.S3AsyncClient;
+import software.amazon.awssdk.services.s3.model.PutObjectResponse;
+import software.amazon.awssdk.transfer.s3.S3TransferManager;
+import software.amazon.awssdk.transfer.s3.model.DownloadFileRequest;
+import software.amazon.awssdk.transfer.s3.model.UploadFileRequest;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class ExportService {
     private static final String S3_PREFIX = "fulldump/organisaatio/v2";
+    private final ObjectMapper objectMapper = new ObjectMapper()
+            .setSerializationInclusion(JsonInclude.Include.NON_NULL);
 
     @Value("${organisaatio.tasks.export.bucket-name}")
     private String bucketName;
+    @Value("${organisaatio.tasks.export.lampi-bucket-name}")
+    private String lampiBucketName;
+    @Value("${organisaatio.tasks.export.upload-to-s3:true}")
+    private boolean uploadToS3;
 
+    private final S3AsyncClient opintopolkuS3Client;
+    private final S3AsyncClient lampiS3Client;
     private final JdbcTemplate jdbcTemplate;
 
     @Transactional
@@ -74,5 +96,56 @@ public class ExportService {
         var sql = "SELECT rows_uploaded FROM aws_s3.query_export_to_s3(?, aws_commons.create_s3_uri(?, ?, ?), options := 'FORMAT CSV, HEADER TRUE')";
         var rowsUploaded = jdbcTemplate.queryForObject(sql, Long.class, query, bucketName, objectKey, OpintopolkuAwsClients.REGION.id());
         log.info("Exported {} rows to S3 object {}", rowsUploaded, objectKey);
+    }
+
+    public void copyExportFilesToLampi() throws IOException {
+        var csvManifest = new ArrayList<ExportManifest.ExportFileDetails>();
+        csvManifest.add(copyFileToLampi(S3_PREFIX + "/csv/organisaatio.csv"));
+        csvManifest.add(copyFileToLampi(S3_PREFIX + "/csv/organisaatiosuhde.csv"));
+        writeManifest(S3_PREFIX + "/csv/manifest.json", new ExportManifest(csvManifest));
+    }
+
+    private ExportManifest.ExportFileDetails copyFileToLampi(String objectKey) throws IOException {
+        var temporaryFile = File.createTempFile("export", ".csv");
+        try {
+            log.info("Downloading file from S3: {}/{}", bucketName, objectKey);
+            try (var downloader = S3TransferManager.builder().s3Client(opintopolkuS3Client).build()) {
+                var fileDownload = downloader.downloadFile(DownloadFileRequest.builder()
+                        .getObjectRequest(b -> b.bucket(bucketName).key(objectKey))
+                        .destination(temporaryFile)
+                        .build());
+                fileDownload.completionFuture().join();
+            }
+
+            var response = uploadFile(lampiS3Client, lampiBucketName, objectKey, temporaryFile);
+            return new ExportManifest.ExportFileDetails(objectKey, response.versionId());
+        } finally {
+            Files.deleteIfExists(temporaryFile.toPath());
+        }
+    }
+
+    private PutObjectResponse uploadFile(S3AsyncClient s3Client, String bucketName, String objectKey, File file) {
+        if (!uploadToS3) {
+            log.info("Skipping upload to S3");
+            return null;
+        }
+        log.info("Uploading file to S3: {}/{}", bucketName, objectKey);
+        try (var uploader = S3TransferManager.builder().s3Client(s3Client).build()) {
+            var fileUpload = uploader.uploadFile(UploadFileRequest.builder()
+                    .putObjectRequest(b -> b.bucket(bucketName).key(objectKey))
+                    .source(file)
+                    .build());
+            var result = fileUpload.completionFuture().join();
+            return result.response();
+        }
+    }
+
+    private void writeManifest(String objectKey, ExportManifest manifest) throws JsonProcessingException {
+        var manifestJson = objectMapper.writeValueAsString(manifest);
+        var response = lampiS3Client.putObject(
+                b -> b.bucket(lampiBucketName).key(objectKey),
+                AsyncRequestBody.fromString(manifestJson)
+        ).join();
+        log.info("Wrote manifest to S3: {}", response);
     }
 }
