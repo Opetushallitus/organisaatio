362d0c8598d33b93a87e90931184c264290b6ed7
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2024-05-08 15:43:15 +0300

    OPHYK-77 fix urls to viestinvalitys UI

--- organisaatio-service/src/main/java/fi/vm/sade/organisaatio/config/UrlConfiguration.java
+++ organisaatio-service/src/main/java/fi/vm/sade/organisaatio/config/UrlConfiguration.java
@@ -1,7 +1,6 @@
 package fi.vm.sade.organisaatio.config;
 
 import fi.vm.sade.properties.OphProperties;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
@@ -9,7 +8,6 @@ import org.springframework.core.env.Environment;
 @PropertySource("classpath:application.properties")
 @Configuration
 public class UrlConfiguration extends OphProperties {
-    @Autowired
     public UrlConfiguration(Environment environment) {
         addFiles("/organisaatio-service-oph.properties");
         this.addOverride("host.virkailija", environment.getRequiredProperty("host.virkailija"));
