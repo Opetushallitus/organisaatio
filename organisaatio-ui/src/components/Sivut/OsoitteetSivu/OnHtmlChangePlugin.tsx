691b3375f9ba2080a140363a0b5a807461898a1a
Henry Heikkinen <henry.heikkinen@reaktor.com>
2024-05-27 14:22:39 +0300

    OPHYK-74 Clean up the generated HTML content

--- organisaatio-ui/src/components/Sivut/OsoitteetSivu/OnHtmlChangePlugin.tsx
+++ organisaatio-ui/src/components/Sivut/OsoitteetSivu/OnHtmlChangePlugin.tsx
@@ -17,10 +17,25 @@ export function OnHtmlChangePlugin({ onChange }: OnHtmlChangePluginProps) {
                 }
                 const text = $getRoot().getTextContent().trim();
                 const html = $generateHtmlFromNodes(editor);
-                onChange(text, html);
+                onChange(text, cleanupHtml(html));
             });
         });
     }, [editor, onChange]);
 
     return null;
+}
+
+function cleanupHtml(html: string) {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, 'text/html');
+
+    // Remove unnecessary spans
+    doc.querySelectorAll('span').forEach((span) => span.replaceWith(...span.childNodes));
+
+    // Remove dir attributes
+    for (const element of doc.querySelectorAll('[dir]')) {
+        element.removeAttribute('dir');
+    }
+
+    return doc.body.innerHTML;
 }
