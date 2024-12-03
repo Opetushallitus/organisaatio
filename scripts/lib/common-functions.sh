f51e2f55f383c88dbfbbf6ba26a5280460e3b133
Henry Heikkinen <henry.heikkinen@reaktor.com>
2024-12-03 10:16:46 +0200

    OPHYK-212 Add missing common-functions.sh file

---
+++ scripts/lib/common-functions.sh
@@ -0,0 +1,53 @@
+export repo="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
+
+function require_docker {
+  require_command docker
+  docker ps >/dev/null 2>&1 || fatal "Running 'docker ps' failed. Is docker daemon running? Aborting."
+}
+
+function require_command {
+  if ! command -v "$1" >/dev/null; then
+    fatal "I require $1 but it's not installed. Aborting."
+  fi
+}
+
+function info {
+  log "INFO" "$1"
+}
+
+function fatal {
+  log "ERROR" "$1"
+  exit 1
+}
+
+function log {
+  local -r level="$1"
+  local -r message="$2"
+  local -r timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+
+  echo >&2 -e "${timestamp} ${level} ${message}"
+}
+
+function parse_env_from_script_name {
+  local -r file_name="$(basename "$0")"
+  if echo "${file_name}" | grep -E -q '.+-([^-]+)\.sh$'; then
+    local -r env="$(echo "${file_name}" | sed -E -e 's|.+-([^-]+)\.sh$|\1|g')"
+    info "Using env $env"
+    echo $env
+  else
+    fatal "Don't call this script directly"
+  fi
+}
+
+function aws {
+  docker run \
+    --platform linux/amd64 \
+    --env AWS_PROFILE \
+    --env AWS_DEFAULT_REGION \
+    --env AWS_CONTAINER_CREDENTIALS_RELATIVE_URI \
+    --volume "${HOME}/.aws:/root/.aws" \
+    --volume "$( pwd ):/aws" \
+    --rm \
+    --interactive \
+    amazon/aws-cli:2.15.21 "$@"
+}
