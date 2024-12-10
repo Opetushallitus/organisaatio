a745b0b17bfa90e621ac37b32aba7e929c42b80b
Henry Heikkinen <henry.heikkinen@reaktor.com>
2024-12-10 11:14:27 +0200

    Open CodePipeline view in browser after starting CI

--- scripts/start-ci.sh
+++ scripts/start-ci.sh
@@ -20,7 +20,15 @@ function start_ci {
   require_docker
   export_aws_credentials "util"
   echo "Starting ci for $1"
-  aws codepipeline start-pipeline-execution --name "Deploy$(capitalize "${env}")"
+  local -r pipeline_name="Deploy$(capitalize "${env}")"
+  aws codepipeline start-pipeline-execution --name "$pipeline_name"
+  open_codepipeline_in_browser "$pipeline_name"
+}
+
+function open_codepipeline_in_browser {
+  local -r pipeline_name="$1"
+  account_id="$( aws sts get-caller-identity --query Account --output text )"
+  open "https://oph-aws-sso.awsapps.com/start/#/console?account_id=${account_id}&role_name=AdministratorAccess&destination=https%3A%2F%2Feu-west-1.console.aws.amazon.com%2Fcodesuite%2Fcodepipeline%2Fpipelines%2F${pipeline_name}%2Fview%3Fregion%3D$( aws configure get region )"
 }
 
 function capitalize {
