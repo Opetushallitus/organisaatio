2d1c2d697f9885df694b7282e61552967e3681f6
Henry Heikkinen <henry.heikkinen@reaktor.com>
2024-12-17 10:42:08 +0200

    Add Gradle lockfile for organisaatio-service to see if Trivy figures out the dependencies

---
+++ scripts/generate-gradle-lockfile.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -o errexit -o nounset -o pipefail
+readonly repo="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
+
+function main {
+  use_correct_jvm_version
+
+  cd "${repo}/organisaatio-service"
+  "${repo}/gradlew" dependencies --write-locks
+}
+
+function use_correct_jvm_version {
+  JAVA_HOME="$( /usr/libexec/java_home -v "21" )"
+  export JAVA_HOME
+}
+
+main "$@"
