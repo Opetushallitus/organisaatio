897d9b922a031fafff1861cfd2f42e0aade9699a
Timo Suomela <timo.suomela@reaktor.com>
2024-12-30 15:41:21 +0200

    OPHYK-318 Periodically export data required by the datantuonti process

---
+++ organisaatio-service/src/main/java/fi/vm/sade/organisaatio/datantuonti/ExportTask.java
@@ -0,0 +1,47 @@
+package fi.vm.sade.organisaatio.datantuonti;
+
+import com.github.kagkarlsson.scheduler.task.ExecutionContext;
+import com.github.kagkarlsson.scheduler.task.FailureHandler;
+import com.github.kagkarlsson.scheduler.task.TaskInstance;
+import com.github.kagkarlsson.scheduler.task.helper.RecurringTask;
+import com.github.kagkarlsson.scheduler.task.schedule.FixedDelay;
+import fi.vm.sade.organisaatio.service.filters.RequestIdFilter;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+
+@Component
+@Slf4j
+@ConditionalOnProperty(value = "organisaatio.tasks.datantuonti.export.enabled", havingValue = "true")
+public class ExportTask extends RecurringTask<Void> {
+    @Autowired
+    @Qualifier(value = "DatantuontiExportService")
+    private ExportService exportService;
+
+    public ExportTask() {
+        super(
+                "DatantuontiExportTask",
+                FixedDelay.of(Duration.ofHours(1)),
+                Void.class,
+                new FailureHandler.OnFailureReschedule<>(FixedDelay.of(Duration.ofHours(1)))
+        );
+        log.info("Creating export task");
+    }
+
+    @Override
+    public void executeRecurringly(TaskInstance<Void> taskInstance, ExecutionContext executionContext) {
+        try {
+            MDC.put("requestId", RequestIdFilter.generateRequestId());
+            log.info("Running organisaatio datantuonti export task");
+            exportService.createSchema();
+            log.info("Organisaatio datantuonti export task completed");
+        } finally {
+            MDC.remove("requestId");
+        }
+    }
+}
