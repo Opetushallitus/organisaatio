9ab0559e008f3d003449fafc360e1f9c7e1640d3
Henry Heikkinen <henry.heikkinen@reaktor.com>
2025-01-13 16:56:45 +0200

    Fix run-organisaatio-service.sh

--- scripts/run-organisaatio-service.sh
+++ scripts/run-organisaatio-service.sh
@@ -5,7 +5,9 @@ source "$( dirname "${BASH_SOURCE[0]}" )/lib/common-functions.sh"
 function main {
   wait_for_local_db_to_be_healthy
   select_java_version "21"
-  cd "$repo/organisaatio-service"
+  cd "${repo}"
+  mvn clean install -DskipTests
+  cd "${repo}/organisaatio-service"
   mvn spring-boot:run \
     -Dspring-boot.run.jvmArguments="-Dspring.config.location=classpath:application.properties,classpath:application-test-envs.properties -Dspring.profiles.active=dev -Dspring.flyway.enabled=true -Durl-virkailija=http://localhost:9000 -Dhost.virkailija=localhost:9000 -Durl-ytj=http://localhost:9000/ytj -Durl-oidservice=http://localhost:9000/oidservice -Dcas.service.organisaatio-service=http://localhost:8080/organisaatio-service-not-available"
 }
