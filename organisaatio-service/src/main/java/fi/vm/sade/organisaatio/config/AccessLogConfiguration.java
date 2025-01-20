2c2048217929bc7dc62128cd88e2960025804760
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2025-01-20 12:45:49 +0200

    enable access log

--- organisaatio-service/src/main/java/fi/vm/sade/organisaatio/config/AccessLogConfiguration.java
+++ organisaatio-service/src/main/java/fi/vm/sade/organisaatio/config/AccessLogConfiguration.java
@@ -1,7 +1,6 @@
 package fi.vm.sade.organisaatio.config;
 
 import ch.qos.logback.access.tomcat.LogbackValve;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
@@ -11,7 +10,6 @@ import org.springframework.context.annotation.Configuration;
 public class AccessLogConfiguration {
 
     @Bean
-    @ConditionalOnProperty(name = "logback.access")
     public WebServerFactoryCustomizer<TomcatServletWebServerFactory> containerCustomizer() {
         return container -> container.addContextCustomizers(context -> {
             LogbackValve logbackValve = new LogbackValve();
