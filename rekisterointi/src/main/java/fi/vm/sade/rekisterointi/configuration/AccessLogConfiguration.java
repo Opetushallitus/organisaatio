54a9c98011da0b8bebda6b6a8748d6c3e30c33ae
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2025-01-29 16:00:17 +0200

    OPHYK-216 add access logging with logback

---
+++ rekisterointi/src/main/java/fi/vm/sade/rekisterointi/configuration/AccessLogConfiguration.java
@@ -0,0 +1,20 @@
+package fi.vm.sade.rekisterointi.configuration;
+
+import ch.qos.logback.access.tomcat.LogbackValve;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AccessLogConfiguration {
+
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> containerCustomizer() {
+        return container -> container.addContextCustomizers(context -> {
+            LogbackValve logbackValve = new LogbackValve();
+            logbackValve.setFilename("logback-access.xml");
+            context.getPipeline().addValve(logbackValve);
+        });
+    }
+}
