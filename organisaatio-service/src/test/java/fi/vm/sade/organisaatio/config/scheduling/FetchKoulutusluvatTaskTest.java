531f4c2762ee48330ea913aba048afce9d2c4a5e
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2025-04-01 12:45:55 +0300

    add missing wiremock stub for oauth2 bearer token

--- organisaatio-service/src/test/java/fi/vm/sade/organisaatio/config/scheduling/FetchKoulutusluvatTaskTest.java
+++ organisaatio-service/src/test/java/fi/vm/sade/organisaatio/config/scheduling/FetchKoulutusluvatTaskTest.java
@@ -1,5 +1,7 @@
 package fi.vm.sade.organisaatio.config.scheduling;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,11 +9,20 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.jdbc.Sql;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import fi.vm.sade.organisaatio.client.Oauth2BearerClient;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+
+import java.io.IOException;
+
+@WireMockTest(httpPort = 28080)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @Sql({"/data/truncate_tables.sql", "/data/basic_organisaatio_data.sql"})
 class FetchKoulutusluvatTaskTest {
+    @Autowired
+    ObjectMapper objectMapper;
     @Autowired
     private FetchKoulutusluvatTask fetchKoulutusluvatTask;
     @Autowired
@@ -20,7 +31,8 @@ class FetchKoulutusluvatTaskTest {
     private JdbcTemplate jdbcTemplate;
 
     @BeforeEach
-    public void beforeEach() {
+    public void beforeEach() throws IOException {
+        mockOauth2TokenRequest();
         fetchKoodistotTask.execute();
     }
 
@@ -37,5 +49,11 @@ class FetchKoulutusluvatTaskTest {
                 " WHERE ytunnus = ?";
         var count = jdbcTemplate.queryForObject(sql, Long.class, ytunnus);
         assertThat(count).isEqualTo(expectedCount);
+    }
+
+    private void mockOauth2TokenRequest() throws IOException {
+        var fakeTokenResponse = new Oauth2BearerClient.Token("", "Bearer", Integer.MAX_VALUE);
+        stubFor(post(urlEqualTo("/kayttooikeus-service/oauth2/token"))
+                .willReturn(aResponse().withStatus(200).withBody(objectMapper.writeValueAsString(fakeTokenResponse))));
     }
 }
