79cede84b181a6ee30b874dbf90e8f88f8ce96ac
Henry Heikkinen <henry.heikkinen@reaktor.com>
2025-11-06 14:28:24 +0200

    OPHYK-884 URL encode the oauth2 token request content properly

--- organisaatio-service/src/main/java/fi/vm/sade/organisaatio/client/Oauth2BearerClient.java
+++ organisaatio-service/src/main/java/fi/vm/sade/organisaatio/client/Oauth2BearerClient.java
@@ -8,9 +8,13 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.net.URI;
+import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -31,24 +35,34 @@ public class Oauth2BearerClient {
         if (cachedToken != null) return cachedToken;
         String tokenUrl = oauth2IssuerUri + "/oauth2/token";
         log.info("refetching oauth2 bearer from " + tokenUrl);
-        String body = "grant_type=client_credentials&client_id="
-                      + clientId
-                      + "&client_secret="
-                      + clientSecret;
         var request = HttpRequest.newBuilder()
                 .uri(URI.create(tokenUrl))
                 .header("Content-Type", "application/x-www-form-urlencoded")
-                .POST(HttpRequest.BodyPublishers.ofString(body))
+                .POST(encodeFormBody(Map.of(
+                        "grant_type", "client_credentials",
+                        "client_id", clientId,
+                        "client_secret", clientSecret
+                )))
                 .build();
         var client = HttpClient.newHttpClient();
         HttpResponse<String> res = client.send(request, HttpResponse.BodyHandlers.ofString());
         if (res.statusCode() != 200) {
             throw new RuntimeException("Oauth2 bearer returned status code " + res.statusCode() + ": " + res.body());
         }
-        log.info("oauth2 bearer body: " + res.body());
         var newToken = objectMapper.readValue(res.body(), Token.class).access_token();
         cachedToken = newToken;
         return newToken;
+    }
+
+    private HttpRequest.BodyPublisher encodeFormBody(Map<String, String> params) {
+        var body = params.entrySet().stream()
+                .map(entry -> encode(entry.getKey()) + "=" + encode(entry.getValue()))
+                .collect(Collectors.joining("&"));
+        return HttpRequest.BodyPublishers.ofString(body);
+    }
+
+    private String encode(String s) {
+        return URLEncoder.encode(s, StandardCharsets.UTF_8);
     }
 
     public void evictOauth2Bearer() {
