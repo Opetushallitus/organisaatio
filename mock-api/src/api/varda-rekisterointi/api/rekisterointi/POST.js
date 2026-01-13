61bb457ea33d9c01ba84da10857bca23ee140b6c
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2026-01-13 16:04:19 +0200

    add jotpa-rekisterointi localisation to mock-api

---
+++ mock-api/src/api/varda-rekisterointi/api/rekisterointi/POST.js
@@ -0,0 +1,3 @@
+module.exports = function (request, response) {
+    response.send(200, 'ok');
+};
--- mock-api/src/app.js
+++ mock-api/src/app.js
@@ -3,23 +3,17 @@ const morganBody = require('morgan-body');
 const bodyParser = require('body-parser');
 const apiMocker = require('connect-api-mocker');
 const xmlparser = require('express-xml-bodyparser');
+const cors = require('cors');
 
 const port = 9000;
 const app = express();
-const cors = require('cors');
-const debug = false;
-if (debug) {
-    app.use(xmlparser());
-    app.use(bodyParser.json());
-    morganBody(app);
-}
-app.use(cors());
-app.use('/', apiMocker('src/api'));
 
-// endpoints used by rekisterointi-ui
-app.post('/varda-rekisterointi/api/rekisterointi', (_, res) => res.send('ok'));
-app.post('/oauth2/token', (_, res) => res.json({ access_token: 'token', expires_in: 12346, token_type: 'Bear' }));
-app.get('/lokalisointi/cxf/rest/v1/localisation', (_, res) => res.send(localisation));
+app.use(cors());
+app.use(xmlparser());
+app.use(bodyParser.json());
+morganBody(app, { maxBodyLength: 100000 });
+
+app.use('/', apiMocker('src/api'));
 
 console.log(`Mock API Server is up and running at: http://localhost:${port}`);
 app.listen(port);
