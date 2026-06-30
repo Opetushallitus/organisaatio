10b7381b0ceb1f0e190afd76ba3a6f4d21635894
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2026-06-30 16:10:29 +0300

    upgrade mock-api express to v5

--- mock-api/src/app.js
+++ mock-api/src/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const morganBody = require('morgan-body');
-const bodyParser = require('body-parser');
 const apiMocker = require('connect-api-mocker');
 const xmlparser = require('express-xml-bodyparser');
 const cors = require('cors');
@@ -10,7 +9,6 @@ const app = express();
 
 app.use(cors());
 app.use(xmlparser());
-app.use(bodyParser.json());
 morganBody(app, { maxBodyLength: 100000 });
 
 app.use('/', apiMocker('src/api'));
