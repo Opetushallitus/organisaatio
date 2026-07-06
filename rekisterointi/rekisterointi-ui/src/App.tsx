02918b3e0f206d88e1dc61d87c026ac7b7e40d45
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2026-07-06 13:54:56 +0300

    replace universal-cookie and normalize.css with local implementations

--- rekisterointi/rekisterointi-ui/src/App.tsx
+++ rekisterointi/rekisterointi-ui/src/App.tsx
@@ -6,7 +6,6 @@ import fi from 'date-fns/locale/fi';
 import sv from 'date-fns/locale/sv';
 
 // import global styles first
-import 'normalize.css';
 import 'oph-virkailija-style-guide/oph-styles.css';
 import 'react-datepicker/dist/react-datepicker.css';
 import './styles.css';
