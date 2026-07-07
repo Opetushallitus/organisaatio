bcd8354eb1013f8bb7da45227b5d584eaf7f8f16
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2026-07-07 15:07:46 +0300

    remove universal-cookie

--- organisaatio-ui/src/index.tsx
+++ organisaatio-ui/src/index.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import axios from 'axios';
-import Cookies from 'universal-cookie';
 import { ROOT_OID } from './contexts/constants';
 import InitializeApp from './components/InitializeApp/InitializeApp';
 import OrganisaatioApp from './OrganisaatioApp';
@@ -11,11 +10,29 @@ import Loading from './components/Loading/Loading';
 import ErrorPage from './components/Sivut/VirheSivu/VirheSivu';
 import { jotaiStore } from './jotaiStore';
 
-const cookies = new Cookies();
+function getCookie(name: string): string | undefined {
+    const prefix = `${name}=`;
+    const cookie = window.document.cookie
+        .split(';')
+        .map((part) => part.trim())
+        .find((part) => part.startsWith(prefix));
+
+    if (!cookie) {
+        return undefined;
+    }
+
+    const value = cookie.slice(prefix.length);
+    try {
+        return decodeURIComponent(value);
+    } catch {
+        return value;
+    }
+}
+
 axios.interceptors.request.use((config) => {
     if (config.headers) {
         config.headers['Caller-Id'] = `${ROOT_OID}.organisaatio-ui`;
-        config.headers['CSRF'] = cookies.get('CSRF');
+        config.headers['CSRF'] = getCookie('CSRF');
     }
     return config;
 });
